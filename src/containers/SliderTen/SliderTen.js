472c0fe342b97e36e7b06fdb61b18d39283b0742
Rob Morgan <rmorgan323@gmail.com>
2018-02-11 13:11:02 -0700

    update sensory component and add FAIR to sliders

--- src/containers/SliderTen/SliderTen.js
+++ src/containers/SliderTen/SliderTen.js
@@ -8,10 +8,11 @@ class SliderTen extends Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
       value: 0,
-      previousValue: 0
+      previousValue: 0,
+      fair: '',
+      previousFair: ''
     };
   }
 
@@ -19,21 +20,26 @@ class SliderTen extends Component {
     this.setState({value})
   }
 
-  handleSliderUpdate = (previousValue) => {
-    if (this.state.value !== previousValue) {
-      this.props.updateSlider({[this.props.databaseName]: this.state.value})
+  handleSliderUpdate = (previousValue, previousFair) => {
+    if (this.state.value !== previousValue || this.state.fair !== previousFair) {
+      this.props.updateSlider({[this.props.databaseName]: this.state.value.toString() + this.state.fair})
     }
   
-    this.setState({previousValue: this.state.value})
+    this.setState({previousValue: this.state.value, previousFair: this.state.fair})
+  }
+
+  chooseFair = (letter) => {
+    this.setState({fair: letter})
   }
 
   render() {
     return (
       <div 
         className="SliderTen"
-        onMouseLeave={() => this.handleSliderUpdate(this.state.previousValue)}
+        onMouseLeave={() => this.handleSliderUpdate(this.state.previousValue, this.state.previousFair)}
       >
         <h5>{this.props.sliderTitle}</h5>
+        <p style={this.state.value === 0 ? {'opacity': 0} : null} >{this.state.value}</p>
         <Range
           value={this.state.value}
           fillColor={{
@@ -56,7 +62,12 @@ class SliderTen extends Component {
           min={0}
           max={10}
         />
-        <p style={this.state.value === 0 ? {'opacity': 0} : null} >{this.state.value}</p>
+        <h2>
+          <span onClick={() => this.chooseFair('f')} className={this.state.fair === 'f' ? "button-fair button-active" : "button-fair"}>F</span>
+          <span onClick={() => this.chooseFair('a')} className={this.state.fair === 'a' ? "button-fair button-active" : "button-fair"}>A</span>
+          <span onClick={() => this.chooseFair('i')} className={this.state.fair === 'i' ? "button-fair button-active" : "button-fair"}>I</span>
+          <span onClick={() => this.chooseFair('r')} className={this.state.fair === 'r' ? "button-fair button-active" : "button-fair"}>R</span>
+        </h2>
       </div>
     );
   }
