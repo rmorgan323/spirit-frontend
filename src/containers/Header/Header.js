9864febeef33c09cf37aa5a432c754409aaa3382
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 14:32:50 -0700

    Build conditional in Header to check if user is logged in

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -9,8 +9,13 @@ const Header = (props) => {
   return (
     <div className="Header">
       <img className="header-wordmark" src="/assets/Spirit-Wordmark-White.png" alt="SpIRiT-wordmark" />
-      <h5 className="header-welcome-user">Welcome, {props.user.name}!</h5>
-      <h5 className="header-welcome-clinic">Clinic: {props.user.clinic}</h5>
+      {
+        props.user &&
+        <div>
+          <h5 className="header-welcome-user">Welcome, {props.user.name}!</h5>
+          <h5 className="header-welcome-clinic">Clinic: {props.user.clinic}</h5>
+        </div>
+      }
     </div>
   )
 }
