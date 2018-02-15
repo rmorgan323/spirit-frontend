2c9d41aa1bf3a830a08abf67020202b65d3f94f7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 16:33:14 -0700

    Add getKeyFromLS to headers of loadProcessById

--- src/helpers/loadProcessById/loadProcessById.js
+++ src/helpers/loadProcessById/loadProcessById.js
@@ -3,7 +3,14 @@ import getKeyFromLS from '../getKeyFromLS';
 const loadProcessById = async processId => {
   try {
     const fetchedProcess = fetch(
-      `http://localhost:3000/api/v1/processes/${processId}`
+      `http://localhost:3000/api/v1/processes/${processId}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        }
+      }
     );
     const jsonProcess = fetchedProcess.json();
 
