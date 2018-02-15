b680bde1b7399a0030327c9d1e506ed115246039
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 14:29:04 -0700

    Add Spirit homepage asset and set base styling for landing page

--- src/containers/Spirit/Spirit.js
+++ src/containers/Spirit/Spirit.js
@@ -5,19 +5,21 @@ class Spirit extends Component {
   constructor() {
     super();
 
-    this.state = {
-
-    }
+    this.state = {};
   }
 
   render() {
     return (
-      <div>
-        <h2>homepage</h2>
+      <div className="Spirit">
         <a href="https://spirit.e1.loginrocket.com/login">LOGIN</a>
-        
+        <img
+          className="homepage-wordmark"
+          src="/assets/Spirit-Homepage-Wordmark-Purple.png"
+          alt="Spirit-Wordmark-with-Tagline"
+        />
+        <h4 className="homepage-brief-descriptor">A clinical reasoning model for ensuring best practice in pediatric occupational therapy.</h4>
       </div>
-    )
+    );
   }
 }
 
