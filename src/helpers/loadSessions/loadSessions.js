2da6be8b38f440e4576c58dd6182cc48715ce260
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 15:28:11 -0700

    Add try/catch to loadSessions

--- src/helpers/loadSessions/loadSessions.js
+++ src/helpers/loadSessions/loadSessions.js
@@ -1,8 +1,16 @@
 const loadSessions = async primaryConcernId => {
-  const fetchedSessions = await fetch(`localhost:3000/api/v1/primary-concerns/${primaryConcernId}/sessions`);
-  const jsonSessions = await fetchedSessions.json();
+  try {
+    const fetchedSessions = await fetch(
+      `localhost:3000/api/v1/primary-concerns/${primaryConcernId}/sessions`
+    );
+    const jsonSessions = await fetchedSessions.json();
 
-  return jsonSessions;
-}
+    return jsonSessions;
+  } catch (error) {
+    throw new Error(
+      `Error fetching sessions by primary concern id ${primaryConcernId}: ${error}`
+    );
+  }
+};
 
 export default loadSessions;
