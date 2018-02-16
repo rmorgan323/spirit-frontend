2f6b9429d94535e899d40e853afb2ebf5d05d5d7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 21:51:59 -0700

    Add header to loadPatientConcernById helper

--- src/helpers/loadPatientConcernById/loadPatientConcernById.js
+++ src/helpers/loadPatientConcernById/loadPatientConcernById.js
@@ -3,7 +3,14 @@ import getKeyFromLS from '../getKeyFromLS';
 const loadPatientConcernById = async concernId => {
   try {
     const fetchedConcern = await fetch(
-      `localhost:3000/api/v1/primary-concerns/${concernId}`
+      `localhost:3000/api/v1/primary-concerns/${concernId}`,
+      {
+        method: 'GET',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        }
+      }
     );
     const jsonConcern = await fetchedConcern.json();
 
