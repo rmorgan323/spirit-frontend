6d401560e1702761253caa55f187de5585a03fd5
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 22:30:19 -0700

    Fix url for loadPatientConcernById

--- src/helpers/loadPatientConcernById/loadPatientConcernById.js
+++ src/helpers/loadPatientConcernById/loadPatientConcernById.js
@@ -3,7 +3,7 @@ import getKeyFromLS from '../getKeyFromLS';
 const loadPatientConcernById = async concernId => {
   try {
     const fetchedConcern = await fetch(
-      `localhost:3000/api/v1/primary-concerns/${concernId}`,
+      `http://localhost:3000/api/v1/primary-concerns/${concernId}`,
       {
         method: 'GET',
         headers: {
@@ -13,7 +13,6 @@ const loadPatientConcernById = async concernId => {
       }
     );
     const jsonConcern = await fetchedConcern.json();
-
     return jsonConcern[0];
   } catch (error) {
     throw new Error(`Error fetch primary concern by id ${concernId}: ${error}`);
