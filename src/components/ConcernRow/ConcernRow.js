cb9f1282bd0b124bf3e389c6014fcbe14a902d64
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 21:12:38 -0700

    Refactor hover from this.state in ConcernRow

--- src/components/ConcernRow/ConcernRow.js
+++ src/components/ConcernRow/ConcernRow.js
@@ -41,20 +41,21 @@ class ConcernRow extends Component {
 
   render() {
     const { id, concern, d1, d2, d3, d4, d5, d6, notes } = this.props;
+    const { hover } = this.state;
 
     return (
       <div className="ConcernRow">
         <p className="concern-left">{concern}</p>
         <p>
-          <span className={ d1 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>1</span>
-          <span className={ d2 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>2</span>
-          <span className={ d3 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>3</span>
-          <span className={ d4 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>4</span>
-          <span className={ d5 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>5</span>
-          <span className={ d6 === false ? "domains" : (this.state.hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>6</span>
+          <span className={ d1 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>1</span>
+          <span className={ d2 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>2</span>
+          <span className={ d3 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>3</span>
+          <span className={ d4 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>4</span>
+          <span className={ d5 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>5</span>
+          <span className={ d6 === false ? "domains" : (hover === false ? "domains domains-true" : "domains domains-true domains-hover") }>6</span>
         </p>
         <p className="concern-notes">{notes}</p>
-        <button onClick={console.log(this.props)} onMouseEnter={() => this.selectEnter()} onMouseOut={() => this.selectLeave()} className="select-concern-button">SELECT</button>
+        <button onClick={console.log(id)} onMouseEnter={() => this.selectEnter()} onMouseOut={() => this.selectLeave()} className="select-concern-button">SELECT</button>
       </div>
     )
   }
