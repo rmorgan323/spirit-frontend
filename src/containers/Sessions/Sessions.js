5458c7f41a0250088c8400a1245a173e8ca42bc3
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-15 20:32:57 -0700

    Mock out Sessions container

---
+++ src/containers/Sessions/Sessions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-router-dom';
+import './Sessions.css';
+
+const Sessions = () => {
+  return (
+    <div className="Sessions">
+      <h1>Sessions</h1>
+    </div>
+  )
+}
+
+export default connect(null, null)(Sessions);
