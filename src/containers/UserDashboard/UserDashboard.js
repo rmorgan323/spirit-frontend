1473b773bf5c5d2180e1c9dd3af6c5fb178fd47f
Rob Morgan <rmorgan323@gmail.com>
2018-02-16 10:07:22 -0700

    update user dashboard page and styling

--- src/containers/UserDashboard/UserDashboard.js
+++ src/containers/UserDashboard/UserDashboard.js
@@ -21,22 +21,24 @@ class UserDashboard extends Component {
   }
 
   handleSubmit = () => {
-    // console.log(this.state.firstInitial, this.state.lastInitial, this.props.user.id, this.props.user.clinic_abbreviation)
-
     this.props.savePatient(this.state.firstInitial, this.state.lastInitial, this.props.user.id, this.props.user.clinic_abbreviation)
   }
 
   render() {
     return (
-      <div>
-        <Link to={`/spirit/users/${this.props.user.id}/create`}>ADD CLINIC</Link>
-        <Link to={`/spirit/users/${this.props.user.id}/join`}>JOIN CLINIC</Link>
-
+      <div className="UserDashboard">
         <h4>Add NEW patient with 2 patient initials</h4>
-        <input maxLength={1} onChange={(event) => this.handleChange(event)} value={this.state.firstInitial.toUpperCase()} name="firstInitial" placeholder="First Initial"></input>
-        <input maxLength={1} onChange={(event) => this.handleChange(event)} value={this.state.lastInitial.toUpperCase()} name="lastInitial" placeholder="Last Initial"></input>
-        <button onClick={() => this.handleSubmit()} type="submit">SUBMIT</button>
+        <div className="input-holder">
+          <input maxLength={1} onChange={(event) => this.handleChange(event)} value={this.state.firstInitial.toUpperCase()} name="firstInitial" placeholder="First Initial"></input>
+          <input maxLength={1} onChange={(event) => this.handleChange(event)} value={this.state.lastInitial.toUpperCase()} name="lastInitial" placeholder="Last Initial"></input>
+          <button onClick={() => this.handleSubmit()} type="submit">SUBMIT</button>
+        </div>
         <PatientList />
+
+        <div className="link-holder">
+          <Link to={`/spirit/users/${this.props.user.id}/create`}>ADD CLINIC</Link>
+          <Link to={`/spirit/users/${this.props.user.id}/join`}>JOIN CLINIC</Link>
+        </div>
       </div>
     )
   }
