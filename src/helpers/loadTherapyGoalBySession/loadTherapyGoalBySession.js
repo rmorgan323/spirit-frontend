d34ddfe6ce65e49101d9a895812f5ff3e7064a95
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-17 14:06:53 -0700

    Build loadTherapyGoalBySession fetch request

---
+++ src/helpers/loadTherapyGoalBySession/loadTherapyGoalBySession.js
@@ -0,0 +1,25 @@
+import getKeyFromLS from '../getKeyFromLS';
+
+const loadTherapyGoalBySession = async sessionId => {
+  try {
+    const fetchedTherapyGoal = await fetch(
+      `http://localhost:3000/api/v1/sessions/${sessionId}/therapy-goals`,
+      {
+        method: 'GET',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    const jsonTherapyGoal = await fetchedTherapyGoal.json();
+
+    return jsonTherapyGoal;
+  } catch (error) {
+    throw new Error(
+      `Error fetching process by session id ${sessionId}: ${error}`
+    );
+  }
+};
+
+export default loadTherapyGoalBySession;
