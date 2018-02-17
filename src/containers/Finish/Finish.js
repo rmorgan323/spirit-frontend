9732bd5a686201c482d9d93e3e6c2c70fd29829a
Rob Morgan <rmorgan323@gmail.com>
2018-02-17 14:07:25 -0700

    add session tabs component and create finish component

---
+++ src/containers/Finish/Finish.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Finish.css';
+
+class Finish extends Component {
+
+  render() {
+    return (
+      <div>
+        finish
+      </div>
+    )
+  }
+}
+
+export default Finish;
