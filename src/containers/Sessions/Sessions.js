21f7999f0f89b3f1bf1f0ebdbb02630f6c3baf69
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-16 17:32:40 -0700

    Build renderedSessions in Sessions component

--- src/containers/Sessions/Sessions.js
+++ src/containers/Sessions/Sessions.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './Sessions.css';
 
-export const Sessions = () => {
+export const Sessions = (props) => {
+  const renderedSessions = props.sessions.map(session => {
+    return (
+      <div className="session">
+        <span className="session-number">Session {session.id}</span>
+        <span className="session-date">Last updated: {session.updated_at}</span>
+        <span className="session-status">{session.completed ? 'Completed' : 'In Progress'}</span>
+        <Link to={`/spirit/sessions/${session.id}/sam`}>
+          <button
+            className="select-session-button"
+            onClick={() => console.log('boom')}
+          >
+            SELECT
+          </button>
+        </Link>
+      </div>
+    )
+  })
   return (
     <div className="Sessions">
       <h1>Sessions</h1>
+      {renderedSessions}
     </div>
   )
 }
 
 const mapStateToProps = store => ({
-  
+  sessions: store.sessions
 })
 
 const mapDispatchToProps = () => ({
