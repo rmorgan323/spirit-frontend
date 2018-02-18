94a23c3d48d0a22c3a7caffad66bc955d1ac9685
Rob Morgan <rmorgan323@gmail.com>
2018-02-18 16:45:56 -0700

    add sam page with clickable links

--- src/containers/Sam/Sam.js
+++ src/containers/Sam/Sam.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Sam.css';
+import { Link } from 'react-router-dom';
 
 class Sam extends Component {
   constructor() {
@@ -9,7 +10,31 @@ class Sam extends Component {
   render() {
     return(
       <div className="Sam">
-      sam
+        <div className="sam-image-holder">
+          <img className="sam-big-image" src="/assets/sam-new.svg" />
+
+          <Link to="modulation">
+          <p className="sam-session-dots session-dots-1">1</p>
+          </Link>
+
+          <Link to="postural">
+          <p className="sam-session-dots session-dots-2">2</p>
+          </Link>
+
+          <Link to="sensory">
+          <p className="sam-session-dots session-dots-3">3</p>
+          </Link>
+
+          <Link to="social">
+          <p className="sam-session-dots session-dots-4">4</p>
+          </Link>
+
+          <Link to="executive">
+          <p className="sam-session-dots session-dots-5">5</p>
+          </Link>
+
+          <p className="sam-session-dots session-dots-6">6</p>
+        </div>
       </div>
     )
   }
