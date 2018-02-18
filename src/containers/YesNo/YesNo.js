a5fc6c0997f550fdbca45a596058b123d3c0fc4d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 12:12:37 -0700

    Fix linting errors in YesNo compoent

--- src/containers/YesNo/YesNo.js
+++ src/containers/YesNo/YesNo.js
@@ -9,53 +9,75 @@ class YesNo extends Component {
 
     this.state = {
       yesNo: null
-    }
+    };
   }
 
   componentDidMount() {
     const { selectedProcess, databaseName } = this.props;
-    const matchedComponentValue = selectedProcess[Object.keys(selectedProcess).find(component => component === databaseName)];
+    const matchedComponentValue =
+      selectedProcess[
+        Object.keys(selectedProcess).find(
+          component => component === databaseName
+        )
+      ];
 
     this.loadComponentValue(matchedComponentValue);
   }
 
   loadComponentValue = matchedComponentValue => {
     if (matchedComponentValue !== null) {
-      const yesNo = matchedComponentValue
-      this.setState({yesNo})
+      const yesNo = matchedComponentValue;
+      this.setState({ yesNo });
     }
-  }
+  };
 
-  handleChange = (boolean) => {
-    this.props.updateProcessPerformanceComponent(this.props.selectedProcess.id, {[this.props.databaseName]: boolean});
-    this.setState({yesNo: boolean})
-  }
+  handleChange = boolean => {
+    const {
+      updateProcessPerformanceComponent,
+      selectedProcess,
+      databaseName
+    } = this.props;
+
+    updateProcessPerformanceComponent(selectedProcess.id, {
+      [databaseName]: boolean
+    });
+    this.setState({ yesNo: boolean });
+  };
 
   render() {
+    const { question } = this.props;
+    const { yesNo } = this.state;
+
     return (
       <div className="YesNo">
-        <h5>{this.props.question}</h5>
+        <h5>{question}</h5>
         <button
-          className={this.state.yesNo === true ? "yes-no yes-no-active" : "yes-no"} 
-          onClick={() => this.handleChange(true)} 
-        ><span className="yes-span">YES</span></button>
-        <button 
-          className={this.state.yesNo === false ? "yes-no yes-no-active" : "yes-no"} 
-          onClick={() => this.handleChange(false)} 
-        ><span className="no-span">NO</span></button>
+          className={yesNo === true ? 'yes-no yes-no-active' : 'yes-no'}
+          onClick={() => this.handleChange(true)}
+        >
+          <span className="yes-span">YES</span>
+        </button>
+        <button
+          className={yesNo === false ? 'yes-no yes-no-active' : 'yes-no'}
+          onClick={() => this.handleChange(false)}
+        >
+          <span className="no-span">NO</span>
+        </button>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = store => ({
   selectedProcess: store.selectedProcess
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   updateProcessPerformanceComponent: (processId, updatedProcess) => {
-    dispatch(actions.updateProcessPerformanceComponent(processId, updatedProcess));
+    dispatch(
+      actions.updateProcessPerformanceComponent(processId, updatedProcess)
+    );
   }
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(YesNo);
