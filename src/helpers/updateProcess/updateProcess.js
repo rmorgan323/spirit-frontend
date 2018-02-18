587a85c9567a194f5173dc2329cd6602d1632dc4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 12:18:14 -0700

    Fix updateProcess helper linting errors

--- src/helpers/updateProcess/updateProcess.js
+++ src/helpers/updateProcess/updateProcess.js
@@ -2,23 +2,23 @@ import getKeyFromLS from '../getKeyFromLS';
 
 const updateProcess = async (processId, updatedProcess) => {
   try {
-    const fetchedProcess = await fetch(`http://localhost:3000/api/v1/processes/${processId}`,
-    {
-      method: 'PUT',
-      headers: {
-        'x-token': getKeyFromLS(),
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(updatedProcess)
-    });
+    const fetchedProcess = await fetch(
+      `http://localhost:3000/api/v1/processes/${processId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedProcess)
+      }
+    );
     const jsonProcess = await fetchedProcess.json();
 
-    return jsonProcess
+    return jsonProcess;
   } catch (error) {
-    throw new Error(
-      `Error updating process by id ${processId}: ${error}`
-    );
+    throw new Error(`Error updating process by id ${processId}: ${error}`);
   }
-}
+};
 
 export default updateProcess;
