dba6dad759317219f5d4c795fb297ae7f9029262
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 11:58:01 -0700

    Pull body parts values from BE to component

--- src/containers/BodyDiagram/BodyDiagram.js
+++ src/containers/BodyDiagram/BodyDiagram.js
@@ -28,6 +28,27 @@ class BodyDiagram extends Component {
     }
   }
 
+  componentDidMount() {
+    const { selectedProcess } = this.props;
+    const bodyPropertiesArray = [ 'pos_4_core', 'pos_4_shoulder', 'pos_4_pelvic', 'pos_4_head', 'pos_4_eyes', 'pos_4_hand', 'pos_4_lower', 'pos_4_foot' ];
+    const matchedComponentValues = bodyPropertiesArray.map(componentName => {
+      const matchedComponent = Object.keys(selectedProcess).find(component => component === componentName);
+
+      return selectedProcess[matchedComponent];
+    });
+
+    console.log(matchedComponentValues)
+
+    // this.loadComponentValue(matchedComponentValue);
+  }
+
+  loadComponentValue = matchedComponentValue => {
+    // if (matchedComponentValue !== null) {
+    //   const yesNo = matchedComponentValue
+    //   this.setState({yesNo})
+    // }
+  }
+
   hoverMe = (num) => {
     this.setState({[`hover${num}`]: true})
   }
@@ -49,57 +70,59 @@ class BodyDiagram extends Component {
   }
 
   render() {
+    const { part1, part2, part3, part4, part5, part6, part7, part8 } = this.state;
+    const { hover1, hover2, hover3, hover4, hover5, hover6, hover7, hover8 } = this.state;
     return (
-      <div 
+      <div
         className="BodyDiagram"
-        onMouseLeave={() => this.handleDataUpdate(this.state.part1, this.state.part2, this.state.part3, this.state.part4, this.state.part5, this.state.part6, this.state.part7, this.state.part8)}
+        onMouseLeave={() => this.handleDataUpdate(part1, part2, part3, part4, part5, part6, part7, part8)}
       >
         <h4 className="category-title">Body Parts</h4>
         <div className="body-key">
-          <h6 
-            className={this.state.part1 || this.state.hover1 ? "hover-purple-color" : null}
+          <h6
+            className={part1 || hover1 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(1)} 
             onMouseOver={() => this.hoverMe(1)} 
             onMouseOut={() => this.unHoverMe(1)} 
           >1. Core Abdominal Control</h6>
           <h6
-            className={this.state.part2 || this.state.hover2 ? "hover-purple-color" : null}
+            className={part2 || hover2 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(2)}
             onMouseOver={() => this.hoverMe(2)} 
             onMouseOut={() => this.unHoverMe(2)} 
           >2. Shoulder Girdle Core</h6>
           <h6
-            className={this.state.part3 || this.state.hover3 ? "hover-purple-color" : null}
+            className={part3 || hover3 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(3)} 
             onMouseOver={() => this.hoverMe(3)} 
             onMouseOut={() => this.unHoverMe(3)} 
           >3. Pelvic Trunk/LE Core</h6>
           <h6
-            className={this.state.part4 || this.state.hover4 ? "hover-purple-color" : null}
+            className={part4 || hover4 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(4)}
             onMouseOver={() => this.hoverMe(4)} 
             onMouseOut={() => this.unHoverMe(4)} 
           >4. Head/Neck to Trunk Core</h6>
           <h6
-            className={this.state.part5 || this.state.hover5 ? "hover-purple-color" : null}
+            className={part5 || hover5 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(5)}
             onMouseOver={() => this.hoverMe(5)} 
             onMouseOut={() => this.unHoverMe(5)}  
           >5. Eyes in Head Core</h6>
           <h6
-            className={this.state.part6 || this.state.hover6 ? "hover-purple-color" : null}
+            className={part6 || hover6 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(6)}
             onMouseOver={() => this.hoverMe(6)} 
             onMouseOut={() => this.unHoverMe(6)} 
           >6. Hand Control</h6>
           <h6
-            className={this.state.part7 || this.state.hover7 ? "hover-purple-color" : null}
+            className={part7 || hover7 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(7)}
             onMouseOver={() => this.hoverMe(7)} 
             onMouseOut={() => this.unHoverMe(7)} 
           >7. Lower Extremity</h6>
           <h6
-            className={this.state.part8 || this.state.hover8 ? "hover-purple-color" : null}
+            className={part8 || hover8 ? "hover-purple-color" : null}
             onClick={() => this.togglePart(8)} 
             onMouseOver={() => this.hoverMe(8)} 
             onMouseOut={() => this.unHoverMe(8)} 
@@ -109,7 +132,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(1)} 
             onMouseOver={() => this.hoverMe(1)} 
             onMouseOut={() => this.unHoverMe(1)} 
-            className={this.state.part1 || this.state.hover1 ? "parts part1 hover-purple-bg" : "parts part1"}
+            className={part1 || hover1 ? "parts part1 hover-purple-bg" : "parts part1"}
           >
             <p>1</p>
           </div>
@@ -117,7 +140,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(2)} 
             onMouseOver={() => this.hoverMe(2)} 
             onMouseOut={() => this.unHoverMe(2)} 
-            className={this.state.part2 || this.state.hover2 ? "parts part2 hover-purple-bg" : "parts part2"}
+            className={part2 || hover2 ? "parts part2 hover-purple-bg" : "parts part2"}
           >
             <p>2</p>
           </div>
@@ -125,7 +148,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(3)} 
             onMouseOver={() => this.hoverMe(3)} 
             onMouseOut={() => this.unHoverMe(3)} 
-            className={this.state.part3 || this.state.hover3 ? "parts part3 hover-purple-bg" : "parts part3"}
+            className={part3 || hover3 ? "parts part3 hover-purple-bg" : "parts part3"}
           >
             <p>3</p>
           </div>
@@ -133,7 +156,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(4)} 
             onMouseOver={() => this.hoverMe(4)} 
             onMouseOut={() => this.unHoverMe(4)} 
-            className={this.state.part4 || this.state.hover4 ? "parts part4 hover-purple-bg" : "parts part4"}
+            className={part4 || hover4 ? "parts part4 hover-purple-bg" : "parts part4"}
           >
             <p>4</p>
           </div>
@@ -141,7 +164,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(5)} 
             onMouseOver={() => this.hoverMe(5)} 
             onMouseOut={() => this.unHoverMe(5)} 
-            className={this.state.part5 || this.state.hover5 ? "parts part5 hover-purple-bg" : "parts part5"}
+            className={part5 || hover5 ? "parts part5 hover-purple-bg" : "parts part5"}
           >
             <p>5</p>
           </div>
@@ -149,7 +172,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(6)} 
             onMouseOver={() => this.hoverMe(6)} 
             onMouseOut={() => this.unHoverMe(6)} 
-            className={this.state.part6 || this.state.hover6 ? "parts part6 hover-purple-bg" : "parts part6"}
+            className={part6 || hover6 ? "parts part6 hover-purple-bg" : "parts part6"}
           >
             <p>6</p>
           </div>
@@ -157,7 +180,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(7)} 
             onMouseOver={() => this.hoverMe(7)} 
             onMouseOut={() => this.unHoverMe(7)} 
-            className={this.state.part7 || this.state.hover7 ? "parts part7 hover-purple-bg" : "parts part7"}
+            className={part7 || hover7 ? "parts part7 hover-purple-bg" : "parts part7"}
           >
             <p>7</p>
           </div>
@@ -165,7 +188,7 @@ class BodyDiagram extends Component {
             onClick={() => this.togglePart(8)} 
             onMouseOver={() => this.hoverMe(8)} 
             onMouseOut={() => this.unHoverMe(8)} 
-            className={this.state.part8 || this.state.hover8 ? "parts part8 hover-purple-bg" : "parts part8"}
+            className={part8 || hover8 ? "parts part8 hover-purple-bg" : "parts part8"}
           >
             <p>8</p>
           </div>
