bfd0697e25c248e7115e2152b7b1b832e43bcd1b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 17:24:18 -0700

    Build UPDATE_TREATMENT_PLAN reducer in selectedTreatmentPlan reducers

--- src/reducers/selectedTreatmentPlan.js
+++ src/reducers/selectedTreatmentPlan.js
@@ -3,9 +3,22 @@ const selectedTreatmentPlan = (store = {}, action) => {
     case 'SELECTED_TREATMENT_PLAN_TO_STORE':
       return action.selectedTreatmentPlan;
 
+    case 'UPDATE_TREATMENT_PLAN':
+      let newTreatmentPlan = store;
+      const updatedKey = Object.keys(action.updatedTreatmentPlan)[0];
+      const updatedValue = Object.values(action.updatedTreatmentPlan)[0];
+
+      Object.keys(newTreatmentPlan).forEach(key => {
+        if (key === updatedKey) {
+          newTreatmentPlan[updatedKey] = updatedValue;
+        }
+      });
+
+      return newTreatmentPlan;
+
     default:
       return store;
   }
-}
+};
 
 export default selectedTreatmentPlan;
