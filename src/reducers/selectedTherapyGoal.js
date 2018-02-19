53d1c7aa8e9d1981a5c8d0e778e344d5a1d4a597
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 13:00:23 -0700

    Add UPDATE_THERAPY_GOAL case to selectedTherapyGoal reducer

--- src/reducers/selectedTherapyGoal.js
+++ src/reducers/selectedTherapyGoal.js
@@ -3,9 +3,20 @@ const selectedTherapyGoal = (store = {}, action) => {
     case 'SELECTED_THERAPY_GOAL_TO_STORE':
       return action.selectedTherapyGoal;
 
+    case 'UPDATE_THERAPY_GOAL':
+      let newTherapyGoal = store;
+      const updatedKeys = Object.keys(action.updatedTherapyGoal);
+      const updatedValues = Object.values(action.updatedTherapyGoal);
+
+      updatedKeys.forEach((key, index) => {
+        newTherapyGoal[key] = updatedValues[index];
+      });
+
+      return newTherapyGoal;
+
     default:
       return store;
   }
-}
+};
 
 export default selectedTherapyGoal;
