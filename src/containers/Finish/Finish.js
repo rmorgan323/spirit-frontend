3e75300ffd03cedbc0c242db7f3b54e20f90e730
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 09:03:50 -0700

    Fix linting errors on Finish component

--- src/containers/Finish/Finish.js
+++ src/containers/Finish/Finish.js
@@ -4,38 +4,34 @@ import TreatmentPlans from '../../containers/TreatmentPlans/TreatmentPlans';
 import TherapyGoals from '../../containers/TherapyGoals/TherapyGoals';
 
 class Finish extends Component {
-
   render() {
     return (
       <div>
         <TreatmentPlans />
-        <TherapyGoals 
-          category="modulation_"
-          title="Modulation"
-          number="1"
-        />
-        <TherapyGoals 
-          category="posture_"
-          title="Postural"
-          number="2"
-        />
-        <TherapyGoals 
+
+        <TherapyGoals category="modulation_" title="Modulation" number="1" />
+
+        <TherapyGoals category="posture_" title="Postural" number="2" />
+
+        <TherapyGoals
           category="sensory_discrimination_"
           title="Sensory Discrimination"
           number="3"
         />
-        <TherapyGoals 
+
+        <TherapyGoals
           category="social_emotional_"
           title="Social/Emotional Development"
           number="4"
         />
-        <TherapyGoals 
+
+        <TherapyGoals
           category="executive_functioning_"
           title="Executive Functioning"
           number="5"
         />
       </div>
-    )
+    );
   }
 }
 
