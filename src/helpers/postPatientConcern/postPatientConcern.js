7639aef362a0b2e1964d8f5af664aa80eb0cb91b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 20:35:05 -0700

    Add try/catch to postPatientConcern helper

--- src/helpers/postPatientConcern/postPatientConcern.js
+++ src/helpers/postPatientConcern/postPatientConcern.js
@@ -1,20 +1,45 @@
 import getKeyFromLS from '../getKeyFromLS';
 
-const postPatientConcern = async ({ description, domain_1, domain_2, domain_3, domain_4, domain_5, domain_6, notes, patient_id }) => {
-  console.log(description, domain_1, domain_2, domain_3, domain_4, domain_5, domain_6, notes, patient_id)
-  const concernId = await fetch(`http://localhost:3000/api/v1/patients/${patient_id}/primary-concerns`, {
-    method: 'POST',
-    headers: {
-      "x-token": getKeyFromLS(),
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      description, domain_1, domain_2, domain_3, domain_4, domain_5, domain_6, notes
-    })
-  });
-  const jsonConcernId = await concernId.json();
+const postPatientConcern = async ({
+  description,
+  domain_1,
+  domain_2,
+  domain_3,
+  domain_4,
+  domain_5,
+  domain_6,
+  notes,
+  patient_id}) => {
 
-  return jsonConcernId[0];
-}
+  try {
+    const concernId = await fetch(
+      `http://localhost:3000/api/v1/patients/${patient_id}/primary-concerns`,
+      {
+        method: 'POST',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          description,
+          domain_1,
+          domain_2,
+          domain_3,
+          domain_4,
+          domain_5,
+          domain_6,
+          notes
+        })
+      }
+    );
+    const jsonConcernId = await concernId.json();
+
+    return jsonConcernId[0];
+  } catch (error) {
+    throw new Error(
+      `Error creating primary concern by patient id ${patient_id}: ${error}`
+    );
+  }
+};
 
 export default postPatientConcern;
