1efbaa54a281982bf3a849ec0042e0e9e511da86
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 10:29:00 -0700

    Fix linting errors and add proptypes to Users container

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import './Users.css';
 
-const Users = (props) =>  {
-
+const Users = props => {
   if (props.user.id) {
     const userId = props.user.id;
-    props.history.push(`/spirit/users/${userId}/`)
-  }  
+    props.history.push(`/spirit/users/${userId}/`);
+  }
 
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div />;
+};
 
 const mapStateToProps = store => ({
   user: store.user
-})
+});
 
 export default connect(mapStateToProps, null)(Users);
+
+Users.propTypes = {
+  user: PropTypes.object,
+  histroy: PropTypes.object
+};
