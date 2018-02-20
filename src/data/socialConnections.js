b1cc45ed8cf4638689a59e9448b44f2b2274888f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 15:50:29 -0700

    Complete socialCOnnections with reference to store/BE

--- src/data/socialConnections.js
+++ src/data/socialConnections.js
@@ -5,23 +5,23 @@ const socialConnections = {
   },
   soc_1_engagement: {
     marked: false,
-    connections: [a1, b1, c1]
+    connections: ['Modulation', 'Postural', 'Sensory']
   },
   soc_1_problem: {
     marked: false,
-    connections: [e17]
+    connections: ['exe_4a_problem']
   },
   soc_1_reciprocity: {
     marked: false,
-    connections: [d10, d15]
+    connections: ['soc_2_self_awareness', 'soc_2_contingent']
   },
   soc_1_reflective: {
     marked: false,
-    connections: [e18]
+    connections: ['exe_4a_reflective']
   },
   soc_1_regulation: {
     marked: false,
-    connections: [a1]
+    connections: ['Modulation']
   },
   soc_1_representation: {
     marked: false,
@@ -37,19 +37,19 @@ const socialConnections = {
   },
   soc_2_contingent: {
     marked: false,
-    connections: [b18]
+    connections: ['pos_5_interactive']
   },
   soc_2_empathy: {
     marked: false,
-    connections: [d14]
+    connections: ['soc_2_inferential']
   },
   soc_2_imitation: {
     marked: false,
-    connections: [b18, c9]
+    connections: ['pos_5_interactive', 'soc_2_imitation']
   },
   soc_2_inferential: {
     marked: false,
-    connections: [d8, e1]
+    connections: ['soc_1_comparative', 'Executive']
   },
   soc_2_joint: {
     marked: false,
@@ -57,71 +57,71 @@ const socialConnections = {
   },
   soc_2_mentalizing: {
     marked: false,
-    connections: [e1]
+    connections: ['Executive']
   },
   soc_2_pro_social: {
     marked: false,
-    connections: [d21, a17]
+    connections: ['soc_2_pro_social', 'mod_3_prosocial']
   },
   soc_2_self_awareness: {
     marked: false,
-    connections: [a1, c1]
+    connections: ['Modulation', 'Sensory']
   },
   soc_2_social_motivators: {
     marked: false,
-    connections: [e15]
+    connections: ['exe_4a_future']
   },
   soc_2_social_perspective: {
     marked: false,
-    connections: [d14]
+    connections: ['soc_2_inferential']
   },
   soc_2_social_referencing: {
     marked: false,
-    connections: [a1, b1, c1]
+    connections: ['Modulation', 'Postural', 'Sensory']
   },
   soc_2_theory: {
     marked: false,
-    connections: [d10]
+    connections: ['soc_2_self_awareness']
   },
   soc_3_activation_high: {
     marked: false,
-    connections: [a1, a10]
+    connections: ['Modulation', 'mod_2_arousability']
   },
   soc_3_activation_low: {
     marked: false,
-    connections: [a1, b1, e1]
+    connections: ['Modulation', 'Postural', 'Executive']
   },
   soc_3_affective: {
     marked: false,
-    connections: [a1]
+    connections: ['Modulation']
   },
   soc_3_emotional_association: {
     marked: false,
-    connections: [e1]
+    connections: ['Executive']
   },
   soc_3_emotional_cascade: {
     marked: false,
-    connections: [a1, a10]
+    connections: ['Modulation', 'mod_2_arousability']
   },
   soc_3_emotional_contagion: {
     marked: false,
-    connections: [a1, e3]
+    connections: ['Modulation', 'exe_1_inhibition']
   },
   soc_3_emotional_differentiation: {
     marked: false,
-    connections: [d10, e1, e3, e10, a1, a10 ]
+    connections: ['soc_2_self_awareness', 'Executive', 'exe_1_inhibition', 'exe_2b_flexibility', 'Modulation', 'mod_2_arousability' ]
   },
   soc_3_self_compassion: {
     marked: false,
-    connections: [e24]
+    connections: ['exe_4b_self_compassion']
   },
   soc_3_self_control: {
     marked: false,
-    connections: [e22]
+    connections: ['exe_4b_self_control']
   },
   soc_3_self_regulation: {
     marked: false,
-    connections: [e23]
+    connections: ['exe_4b_self_regulation']
   },
   soc_4_attachments: {
     marked: false,
