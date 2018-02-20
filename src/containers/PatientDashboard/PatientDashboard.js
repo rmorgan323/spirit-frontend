975a1b7b6a53d7bdad401bde9993c79517ddcbc3
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 09:16:54 -0700

    Destructure variables from this.state in PatientDashboard container

--- src/containers/PatientDashboard/PatientDashboard.js
+++ src/containers/PatientDashboard/PatientDashboard.js
@@ -20,22 +20,34 @@ class PatientDashboard extends Component {
       domain5: false,
       domain6: false,
       notesInput: ''
-    }
+    };
   }
 
-  toggleDomain = (val) => {
-    this.state[val] === false ? this.setState({[val]: true}) : this.setState({[val]: false})
-  }
+  toggleDomain = val => {
+    this.state[val] === false
+      ? this.setState({ [val]: true })
+      : this.setState({ [val]: false });
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { value, name } = event.target;
-    this.setState({[name]: value })
-  }
+    this.setState({ [name]: value });
+  };
 
   handleSave = (concern, d1, d2, d3, d4, d5, d6, notes, id) => {
-    this.props.addConcern(id, { description: concern, domain_1: d1, domain_2: d2, domain_3: d3, domain_4: d4, domain_5: d5, domain_6: d6, notes: notes, patient_id: id });
+    this.props.addConcern(id, {
+      description: concern,
+      domain_1: d1,
+      domain_2: d2,
+      domain_3: d3,
+      domain_4: d4,
+      domain_5: d5,
+      domain_6: d6,
+      notes: notes,
+      patient_id: id
+    });
     this.clearInputs();
-  }
+  };
 
   clearInputs = () => {
     this.setState({
@@ -47,10 +59,23 @@ class PatientDashboard extends Component {
       domain5: false,
       domain6: false,
       notesInput: ''
-    })
-  }
+    });
+  };
 
   render() {
+    const {
+      concernInput,
+      domain1,
+      domain2,
+      domain3,
+      domain4,
+      domain5,
+      domain6,
+      notesInput
+    } = this.state;
+
+    const { currentPatient } = this.props;
+
     return (
       <div className="PatientDashboard">
         <div className="pd-header-row">
@@ -62,50 +87,96 @@ class PatientDashboard extends Component {
         <Concerns />
 
         <div className="pd-input-row">
-          <input 
-            className="primary-concern" 
-            placeholder="Add Concern" 
-            value={this.state.concernInput}
-            onChange={(event) => this.handleChange(event)}
+          <input
+            className="primary-concern"
+            placeholder="Add Concern"
+            value={concernInput}
+            onChange={event => this.handleChange(event)}
             name="concernInput"
-          ></input>
+          />
+
           <p>
-            <span 
-              onClick={(event) => this.toggleDomain('domain1')} 
-              className={this.state.domain1 === false ? "domains" : "domains sam-1 domains-true"}>1
+            <span
+              onClick={event => this.toggleDomain('domain1')}
+              className={
+                domain1 === false ? 'domains' : 'domains sam-1 domains-true'
+              }
+            >
+              1
             </span>
-            <span 
-              onClick={(event) => this.toggleDomain('domain2')} 
-              className={this.state.domain2 === false ? "domains" : "domains sam-2 domains-true"}>2
+
+            <span
+              onClick={event => this.toggleDomain('domain2')}
+              className={
+                domain2 === false ? 'domains' : 'domains sam-2 domains-true'
+              }
+            >
+              2
             </span>
-            <span 
-              onClick={(event) => this.toggleDomain('domain3')} 
-              className={this.state.domain3 === false ? "domains" : "domains sam-3 domains-true"}>3
+
+            <span
+              onClick={event => this.toggleDomain('domain3')}
+              className={
+                domain3 === false ? 'domains' : 'domains sam-3 domains-true'
+              }
+            >
+              3
             </span>
-            <span 
-              onClick={(event) => this.toggleDomain('domain4')} 
-              className={this.state.domain4 === false ? "domains" : "domains sam-4 domains-true"}>4
+
+            <span
+              onClick={event => this.toggleDomain('domain4')}
+              className={
+                domain4 === false ? 'domains' : 'domains sam-4 domains-true'
+              }
+            >
+              4
             </span>
-            <span 
-              onClick={(event) => this.toggleDomain('domain5')} 
-              className={this.state.domain5 === false ? "domains" : "domains sam-5 domains-true"}>5
+
+            <span
+              onClick={event => this.toggleDomain('domain5')}
+              className={
+                domain5 === false ? 'domains' : 'domains sam-5 domains-true'
+              }
+            >
+              5
             </span>
-            <span 
-              onClick={(event) => this.toggleDomain('domain6')} 
-              className={this.state.domain6 === false ? "domains" : "domains sam-6 domains-true"}>6
+
+            <span
+              onClick={event => this.toggleDomain('domain6')}
+              className={
+                domain6 === false ? 'domains' : 'domains sam-6 domains-true'
+              }
+            >
+              6
             </span>
           </p>
-          <input 
+
+          <input
             className="primary-concern-notes"
-            value={this.state.notesInput}
+            value={notesInput}
             placeholder="Add Notes"
-            onChange={(event) => this.handleChange(event)}
+            onChange={event => this.handleChange(event)}
             name="notesInput"
-          ></input>
-          <button 
+          />
+
+          <button
             className="save-primary-concern"
-            onClick={() => this.handleSave(this.state.concernInput, this.state.domain1, this.state.domain2, this.state.domain3, this.state.domain4, this.state.domain5, this.state.domain6, this.state.notesInput, this.props.currentPatient.id)}
-          >save</button>
+            onClick={() =>
+              this.handleSave(
+                concernInput,
+                domain1,
+                domain2,
+                domain3,
+                domain4,
+                domain5,
+                domain6,
+                notesInput,
+                currentPatient.id
+              )
+            }
+          >
+            save
+          </button>
         </div>
 
         <div className="sam-holder">
@@ -118,15 +189,14 @@ class PatientDashboard extends Component {
           <div className="sam-dot-6 sam-dots">6</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = store => ({
   patientConcerns: store.patientConcerns,
   currentPatient: store.currentPatient
-})
-
+});
 
 const mapDispatchToProps = dispatch => ({
   addConcern: (patientId, concernObject) => {
