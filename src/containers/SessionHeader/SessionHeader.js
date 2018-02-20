41cae7a7316ac8b32a765c338c22699a14ea663c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 09:37:59 -0700

    Fix linting errors for SessionHeader component

--- src/containers/SessionHeader/SessionHeader.js
+++ src/containers/SessionHeader/SessionHeader.js
@@ -4,25 +4,24 @@ import { PropTypes } from 'prop-types';
 import './SessionHeader.css';
 
 const SessionHeader = ({ title, headings, databaseNamesArray }) => {
-
   const headingsDisplay = headings.map((heading, index) => {
     return (
       <div key={index} className="slider-holder">
-        <SliderTen 
+        <SliderTen
           sliderTitle={heading}
           databaseName={databaseNamesArray[index]}
         />
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div className="SessionHeader">
       <h3 className="session-title">{title}</h3>
       {headingsDisplay}
     </div>
-  )
-}
+  );
+};
 
 export default SessionHeader;
 
