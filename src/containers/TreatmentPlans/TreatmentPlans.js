dc68d7345a3d17430b0eba7b4c3c14cd37c0a7d6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 10:20:22 -0700

    Fix linting errors in TreatmentPlan component

--- src/containers/TreatmentPlans/TreatmentPlans.js
+++ src/containers/TreatmentPlans/TreatmentPlans.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './TreatmentPlans.css';
 import TreatmentPlanModule from '../TreatmentPlanModule/TreatmentPlanModule';
+import './TreatmentPlans.css';
 
 class TreatmentPlans extends Component {
   constructor() {
@@ -8,76 +8,127 @@ class TreatmentPlans extends Component {
 
     this.state = {
       currentTab: 'modulation'
-    }
+    };
   }
 
-  handleTabChange = (name) => {
-    this.setState({currentTab: name})
-  }
+  handleTabChange = name => {
+    this.setState({ currentTab: name });
+  };
 
   render() {
     return (
       <div className="TreatmentPlans">
-
         <div className="treatment-header-tabs">
           <div
-            className={this.state.currentTab === 'modulation' ? 'treatment-header treatment-header-active treatment-header-modulation' : 'treatment-header treatment-header-modulation'}
-            onClick={() => this.handleTabChange('modulation')}>
+            className={
+              this.state.currentTab === 'modulation'
+                ? 'treatment-header treatment-header-active treatment-header-modulation'
+                : 'treatment-header treatment-header-modulation'
+            }
+            onClick={() => this.handleTabChange('modulation')}
+          >
             <h4>Modulation</h4>
           </div>
-          <div 
-            className={this.state.currentTab === 'postural' ? 'treatment-header treatment-header-active' : 'treatment-header'}
-            onClick={() => this.handleTabChange('postural')}>
+
+          <div
+            className={
+              this.state.currentTab === 'postural'
+                ? 'treatment-header treatment-header-active'
+                : 'treatment-header'
+            }
+            onClick={() => this.handleTabChange('postural')}
+          >
             <h4>Postural</h4>
           </div>
-          <div 
-            className={this.state.currentTab === 'sensory' ? 'treatment-header treatment-header-active' : 'treatment-header'}
-            onClick={() => this.handleTabChange('sensory')}>
+
+          <div
+            className={
+              this.state.currentTab === 'sensory'
+                ? 'treatment-header treatment-header-active'
+                : 'treatment-header'
+            }
+            onClick={() => this.handleTabChange('sensory')}
+          >
             <h4>Sensory Discrimination</h4>
           </div>
-          <div 
-            className={this.state.currentTab === 'social' ? 'treatment-header treatment-header-active' : 'treatment-header'}
-            onClick={() => this.handleTabChange('social')}>
+
+          <div
+            className={
+              this.state.currentTab === 'social'
+                ? 'treatment-header treatment-header-active'
+                : 'treatment-header'
+            }
+            onClick={() => this.handleTabChange('social')}
+          >
             <h4>Social/Emotional Development</h4>
           </div>
-          <div 
-            className={this.state.currentTab === 'executive' ? 'treatment-header treatment-header-active treatment-header-executive' : 'treatment-header treatment-header-executive'}
-            onClick={() => this.handleTabChange('executive')}>
+
+          <div
+            className={
+              this.state.currentTab === 'executive'
+                ? 'treatment-header treatment-header-active treatment-header-executive'
+                : 'treatment-header treatment-header-executive'
+            }
+            onClick={() => this.handleTabChange('executive')}
+          >
             <h4>Executive Functioning</h4>
           </div>
         </div>
 
         <div className="module-container">
-          <div className={this.state.currentTab === 'modulation' ? 'treatment-body' : 'treatment-body display-none'}>
-            <TreatmentPlanModule 
-              type={'modulation_'}
-            />
+          <div
+            className={
+              this.state.currentTab === 'modulation'
+                ? 'treatment-body'
+                : 'treatment-body display-none'
+            }
+          >
+            <TreatmentPlanModule type={'modulation_'} />
           </div>
-          <div className={this.state.currentTab === 'postural' ? 'treatment-body' : 'treatment-body display-none'}>
-            <TreatmentPlanModule 
-              type={'posture_'}
-            />
+
+          <div
+            className={
+              this.state.currentTab === 'postural'
+                ? 'treatment-body'
+                : 'treatment-body display-none'
+            }
+          >
+            <TreatmentPlanModule type={'posture_'} />
           </div>
-          <div className={this.state.currentTab === 'sensory' ? 'treatment-body' : 'treatment-body display-none'}>
-            <TreatmentPlanModule 
-              type={'sensory_discrimination_'}
-            />
+
+          <div
+            className={
+              this.state.currentTab === 'sensory'
+                ? 'treatment-body'
+                : 'treatment-body display-none'
+            }
+          >
+            <TreatmentPlanModule type={'sensory_discrimination_'} />
           </div>
-          <div className={this.state.currentTab === 'social' ? 'treatment-body' : 'treatment-body display-none'}>
-            <TreatmentPlanModule 
-              type={'social_emotional_'}
-            />
+
+          <div
+            className={
+              this.state.currentTab === 'social'
+                ? 'treatment-body'
+                : 'treatment-body display-none'
+            }
+          >
+            <TreatmentPlanModule type={'social_emotional_'} />
           </div>
-          <div className={this.state.currentTab === 'executive' ? 'treatment-body' : 'treatment-body display-none'}>
-            <TreatmentPlanModule 
-              type={'executive_functioning_'}
-            />
+
+          <div
+            className={
+              this.state.currentTab === 'executive'
+                ? 'treatment-body'
+                : 'treatment-body display-none'
+            }
+          >
+            <TreatmentPlanModule type={'executive_functioning_'} />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default TreatmentPlans;
-      
