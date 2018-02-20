0efe151e9eb5642cc1149e2a35c8d98babd4091f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 09:23:39 -0700

    Fix linting errors in Sam component

--- src/containers/Sam/Sam.js
+++ src/containers/Sam/Sam.js
@@ -1,54 +1,42 @@
 import React, { Component } from 'react';
-import './Sam.css';
 import { Link } from 'react-router-dom';
+import './Sam.css';
 
 class Sam extends Component {
-
   render() {
-    return(
+    return (
       <div className="Sam">
         <div className="sam-image-holder">
-          <img className="sam-big-image" src="/assets/sam-new.svg" alt="sam-diagram" />
+          <img
+            className="sam-big-image"
+            src="/assets/sam-new.svg"
+            alt="sam-diagram"
+          />
 
           <Link to="modulation">
-          <p className="sam-session-dots session-dots-1">1</p>
+            <p className="sam-session-dots session-dots-1">1</p>
           </Link>
 
           <Link to="postural">
-          <p className="sam-session-dots session-dots-2">2</p>
+            <p className="sam-session-dots session-dots-2">2</p>
           </Link>
 
           <Link to="sensory">
-          <p className="sam-session-dots session-dots-3">3</p>
+            <p className="sam-session-dots session-dots-3">3</p>
           </Link>
 
           <Link to="social">
-          <p className="sam-session-dots session-dots-4">4</p>
+            <p className="sam-session-dots session-dots-4">4</p>
           </Link>
 
           <Link to="executive">
-          <p className="sam-session-dots session-dots-5">5</p>
+            <p className="sam-session-dots session-dots-5">5</p>
           </Link>
 
           <p className="sam-session-dots session-dots-6">6</p>
         </div>
       </div>
-    )
+    );
   }
 }
 export default Sam;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
