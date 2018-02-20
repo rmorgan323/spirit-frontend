f4b6e7924e7a7fe4ba896116e4f2aaffb93d6f06
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 07:59:37 -0700

    Add try/catch to joinClinic helper

--- src/helpers/joinClinic/joinClinic.js
+++ src/helpers/joinClinic/joinClinic.js
@@ -1,17 +1,24 @@
 import getKeyFromLS from '../getKeyFromLS';
 
 const joinClinic = async (passcode, userId) => {
-  const clinicObject = await fetch(`http://localhost:3000/api/v1/users/${userId}/join`, {
-    method: 'PUT',
-    headers: {
-      "x-token": getKeyFromLS(),
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(passcode)
-  });
-  const jsonClinicObject = await clinicObject.json();
+  try {
+    const clinicObject = await fetch(
+      `http://localhost:3000/api/v1/users/${userId}/join`,
+      {
+        method: 'PUT',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(passcode)
+      }
+    );
+    const jsonClinicObject = await clinicObject.json();
 
-  return jsonClinicObject;
-}
+    return jsonClinicObject;
+  } catch (error) {
+    throw new Error(`Error fetching user by id ${userId}: ${error}`);
+  }
+};
 
 export default joinClinic;
