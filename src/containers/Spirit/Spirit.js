fa0071246df27218b24c5b076e0ab46c40124cb7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 09:53:24 -0700

    Convert Spirit component from class to const

--- src/containers/Spirit/Spirit.js
+++ src/containers/Spirit/Spirit.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Spirit.css';
 
-class Spirit extends Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="Spirit">
-        <img
-          className="homepage-wordmark"
-          src="/assets/Spirit-Homepage-Wordmark-Purple.png"
-          alt="Spirit-Wordmark-with-Tagline"
-        />
-        <h4 className="homepage-brief-descriptor">
-          A clinical reasoning model for ensuring best practice in pediatric
-          occupational therapy.
-        </h4>
-      </div>
-    );
-  }
-}
+const Spirit = () => {
+  return (
+    <div className="Spirit">
+      <img
+        className="homepage-wordmark"
+        src="/assets/Spirit-Homepage-Wordmark-Purple.png"
+        alt="Spirit-Wordmark-with-Tagline"
+      />
+      <h4 className="homepage-brief-descriptor">
+        A clinical reasoning model for ensuring best practice in pediatric
+        occupational therapy.
+      </h4>
+    </div>
+  );
+};
 
 export default Spirit;
