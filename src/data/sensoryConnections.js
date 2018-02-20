bcef44add1d4e2512a3717e328a70ff95ac7de73
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 15:29:47 -0700

    Complete sensoryConnections with reference to store/BE

--- src/data/sensoryConnections.js
+++ src/data/sensoryConnections.js
@@ -1,163 +1,163 @@
 const sensoryConnections = {
   sen_1_ability: {
     marked: false,
-    connections: [c1, c10, e9, c32]
+    connections: ['sen_2_affordance', 'exe_2b_global', 'sen_6_visual']
   },
   sen_1_classification: {
     marked: false,
-    connections: [c2, e1]
+    connections: ['sen_1_ability', 'Executive']
   },
   sen_1_figure: {
     marked: false,
-    connections: [c2, c32]
+    connections: ['sen_1_ability', 'sen_6_visual']
   },
   sen_1_inferences: {
     marked: false,
-    connections: [c10]
+    connections: ['sen_2_affordance']
   },
   sen_1_match: {
     marked: false,
-    connections: [c1, c2]
+    connections: ['sen_1_ability']
   },
   sen_1_spatial: {
     marked: false,
-    connections: [e16, c32]
+    connections: ['exe_4a_spatial', 'sen_6_visual']
   },
   sen_2_affordance: {
     marked: false,
-    connections: [c7]
+    connections: ['sen_1_inferences']
   },
   sen_2_generativity: {
     marked: false,
-    connections: [e1]
+    connections: ['Executive']
   },
   sen_2_imitation: {
     marked: false,
-    connections: [b18, c9, d13]
+    connections: ['pos_5_interactive', 'sen_2_imitation', 'soc_2_imitation']
   },
   sen_2_initiation: {
     marked: false,
-    connections: [f11]
+    connections: ['exe_3_initiation']
   },
   sen_3_modification: {
     marked: false,
-    connections: [e14]
+    connections: ['exe_3_monitoring']
   },
   sen_3_schema_expansion: {
     marked: false,
-    connections: [c11, e1]
+    connections: ['sen_2_generativity', 'Executive']
   },
   sen_3_schema_selection: {
     marked: false,
-    connections: [f1]
+    connections: ['Executive']
   },
   sen_3_sequencing: {
     marked: false,
-    connections: [e13]
+    connections: ['exe_3_sequencing']
   },
   sen_3_sequencing_gestalt: {
     marked: false,
-    connections: [e9]
+    connections: ['exe_2b_global']
   },
   sen_4_control: {
     marked: false,
-    connections: [b1]
+    connections: ['Postural']
   },
   sen_4_monitoring: {
     marked: false,
-    connections: [e14, c16]
+    connections: ['exe_3_monitoring', 'sen_3_modification']
   },
   sen_4_spatial: {
     marked: false,
-    connections: [e16]
+    connections: ['exe_4a_spatial']
   },
   sen_4_temporal: {
     marked: false,
-    connections: [e16]
+    connections: ['exe_4a_spatial']
   },
   sen_4_timing: {
     marked: false,
-    connections: [e16, b1]
+    connections: ['exe_4a_spatial', 'Postural']
   },
   sen_4_timing_force: {
     marked: false,
-    connections: [b1]
+    connections: ['Postural']
   },
   sen_5_fine: {
     marked: false,
-    connections: [b1, c25, c26]
+    connections: ['Postural', 'sen_5_postural', 'sen_5_limb']
   },
   sen_5_gross: {
     marked: false,
-    connections: [b1]
+    connections: ['Postural']
   },
   sen_5_limb: {
     marked: false,
-    connections: [c27, c28, c25, b1]
+    connections: ['sen_5_gross', 'sen_5_fine', 'sen_5_postural', 'Postural']
   },
   sen_5_ocular: {
     marked: false,
-    connections: [b1, e2]
+    connections: ['Postural', 'exe_1_orient']
   },
   sen_5_oral: {
     marked: false,
-    connections: [b1, b11]
+    connections: ['Postural', 'pos_5_suck']
   },
   sen_5_postural: {
     marked: false,
-    connections: [b1]
+    connections: ['Postural']
   },
   sen_5_projected: {
     marked: false,
-    connections: [b1]
+    connections: ['Postural']
   },
   sen_5_tool: {
     marked: false,
-    connections: [b1, c25, c26]
+    connections: ['Postural', 'sen_5_postural', 'sen_5_limb']
   },
   sen_6_bmc: {
     marked: false,
-    connections: [b1, c36, c37, c38]
+    connections: ['Postural', 'sen_h_vestibular', 'sen_h_proprioception', 'sen_h_tactile']
   },
   sen_6_generalized: {
     marked: false,
-    connections: [b1, e1]
+    connections: ['Postural', 'Executive']
   },
   sen_6_praxis: {
     marked: false,
-    connections: [e1]
+    connections: ['Executive']
   },
   sen_6_somatodyspraxia: {
     marked: false,
-    connections: [b1, c36, c37, c38]
+    connections: ['Postural', 'sen_h_vestibular', 'sen_h_proprioception', 'sen_h_tactile']
   },
   sen_6_visual: {
     marked: false,
-    connections: [b1, c2, c3, c4, c5]
+    connections: ['Postural', 'sen_1_ability', 'sen_1_match', 'sen_1_classification', 'sen_1_spatial']
   },
   sen_h_auditory: {
     marked: false,
-    connections: [a1, b1, c1, d1, e1]
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   },
   sen_h_intero: {
     marked: false,
-    connections: [a1, b1, c1, d1, e1]
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   },
   sen_h_proprioception: {
     marked: false,
-    connections: [a1, b1, c1, d1, e1]
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   },
   sen_h_tactile: {
     marked: false,
-    connections: [a1, b1, c1, d1, e1]
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   },
   sen_h_vestibular: {
     marked: false,
-    connections: []
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   },
   sen_h_visual: {
     marked: false,
-    connections: [a1, b1, c1, d1, e1]
+    connections: ['Modulation', 'Postural', 'Social', 'Executive']
   }
 };
 
