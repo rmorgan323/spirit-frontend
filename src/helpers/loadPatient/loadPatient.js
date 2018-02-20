58a8d4c3b9a6156dc7b528b4187dbef0ca69bf8d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 20:26:44 -0700

    Add try/catch to loadPatient

--- src/helpers/loadPatient/loadPatient.js
+++ src/helpers/loadPatient/loadPatient.js
@@ -1,16 +1,20 @@
 import getKeyFromLS from '../getKeyFromLS';
 
-const loadPatient = async (id) => {
-  const patient = await fetch(`http://localhost:3000/api/v1/patients/${id}`, {
-    method: 'GET',
-    headers: {
-      "x-token": getKeyFromLS(),
-      "Content-Type": "application/json"
-    }
-  });
-  const jsonPatient = await patient.json();
+const loadPatient = async id => {
+  try {
+    const patient = await fetch(`http://localhost:3000/api/v1/patients/${id}`, {
+      method: 'GET',
+      headers: {
+        'x-token': getKeyFromLS(),
+        'Content-Type': 'application/json'
+      }
+    });
+    const jsonPatient = await patient.json();
 
-  return jsonPatient[0];
-}
+    return jsonPatient[0];
+  } catch (error) {
+    throw new Error(`Error fetching patient by id ${id}: ${error}`);
+  }
+};
 
 export default loadPatient;
