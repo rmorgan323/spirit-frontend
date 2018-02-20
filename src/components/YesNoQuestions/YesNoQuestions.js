e6726be7d8c75d505a4ab5c71efa7f3b071fa964
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 21:32:26 -0700

    Fix linting errors in YesNoQuestions component

--- src/components/YesNoQuestions/YesNoQuestions.js
+++ src/components/YesNoQuestions/YesNoQuestions.js
@@ -3,7 +3,6 @@ import './YesNoQuestions.css';
 import YesNo from '../../containers/YesNo/YesNo';
 
 const YesNoQuestions = ({ title, questionsArray, databaseNamesArray }) => {
-
   const display = questionsArray.map((question, index) => {
     return (
       <YesNo
@@ -11,15 +10,15 @@ const YesNoQuestions = ({ title, questionsArray, databaseNamesArray }) => {
         databaseName={databaseNamesArray[index]}
         key={index}
       />
-    )
-  })
+    );
+  });
 
   return (
     <div className="YesNoQuestions">
       <h4 className="category-title">{title}</h4>
       {display}
     </div>
-  )
-}
+  );
+};
 
 export default YesNoQuestions;
