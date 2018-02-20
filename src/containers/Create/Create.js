14c660647f5afeb095530c53c759506f4a925c79
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 08:57:12 -0700

    Fix linting errors and add proptypes to Create container

--- src/containers/Create/Create.js
+++ src/containers/Create/Create.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import * as actions from '../../actions';
+import generator from 'generate-password';
 import './Create.css';
-import generator from 'generate-password'
 
 class Create extends Component {
   constructor() {
@@ -13,13 +14,13 @@ class Create extends Component {
       clinicName: '',
       clinicAbbreviation: '',
       successMessage: ''
-    }
+    };
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { name, value } = event.target;
-    this.setState({[name]: value})
-  }
+    this.setState({ [name]: value });
+  };
 
   saveNewClinic = (event, name, abbr) => {
     event.preventDefault();
@@ -28,68 +29,96 @@ class Create extends Component {
       uppercase: false,
       numbers: true,
       excludeSimilarCharacters: true
-    })
+    });
 
-    console.log(password)
-    this.props.saveClinic({name: name, abbreviation: abbr, passcode: password}, this.props.user.id);
-  }
+    this.props.saveClinic(
+      { name: name, abbreviation: abbr, passcode: password },
+      this.props.user.id
+    );
+  };
 
   displayClinic = () => {
     if (Object.keys(this.props.user).length) {
       return (
         <div>
-          <h4><span className="clinic-span">You are a member of: </span>{this.props.user.clinic}</h4>
-          <h4><span className="clinic-span">Clinic Abbreviation: </span>{this.props.user.clinic_abbreviation}</h4>
-          <h4><span className="clinic-span">Clinic Passcode: </span>{this.props.user.clinic_passcode}</h4>
+          <h4>
+            <span className="clinic-span">You are a member of: </span>
+            {this.props.user.clinic}
+          </h4>
+          <h4>
+            <span className="clinic-span">Clinic Abbreviation: </span>
+            {this.props.user.clinic_abbreviation}
+          </h4>
+          <h4>
+            <span className="clinic-span">Clinic Passcode: </span>
+            {this.props.user.clinic_passcode}
+          </h4>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render() {
     return (
       <div className="Create">
-
         {this.displayClinic()}
 
         <form>
           <input
             className="input-clinic-name"
-            onChange={(event) => this.handleChange(event)} 
-            value={this.state.clinicName} 
-            name="clinicName" 
+            onChange={event => this.handleChange(event)}
+            value={this.state.clinicName}
+            name="clinicName"
             placeholder="Add NEW Clinic"
-            maxLength={30}>
-          </input>
-          <input 
+            maxLength={30}
+          />
+          <input
             className="input-abbreviation"
-            onChange={(event) => this.handleChange(event)} 
-            value={this.state.clinicAbbreviation} 
-            name="clinicAbbreviation" 
+            onChange={event => this.handleChange(event)}
+            value={this.state.clinicAbbreviation}
+            name="clinicAbbreviation"
             placeholder="Choose a 3-letter abbreviation for your clinic"
-            maxLength={3}>
-          </input>
-          <button 
+            maxLength={3}
+          />
+          <button
             className="submit-button"
             type="submit"
-            onClick={(event) => this.saveNewClinic(event, this.state.clinicName, this.state.clinicAbbreviation)}
-          >SUBMIT</button>
+            onClick={event =>
+              this.saveNewClinic(
+                event,
+                this.state.clinicName,
+                this.state.clinicAbbreviation
+              )
+            }
+          >
+            SUBMIT
+          </button>
         </form>
 
-        <NavLink className="join-link" to={`/spirit/users/${this.props.user.id}/join`}>JOIN EXISTING CLINIC</NavLink>
+        <NavLink
+          className="join-link"
+          to={`/spirit/users/${this.props.user.id}/join`}
+        >
+          JOIN EXISTING CLINIC
+        </NavLink>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = store => ({
   user: store.user
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   saveClinic: (clinic, userId) => {
     dispatch(actions.saveClinic(clinic, userId));
   }
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Create);
+
+Create.propTypes = {
+  saveClinic: PropTypes.func,
+  user: PropTypes.object
+};
