ac48b21b4159be93e107ec87cbeda1498a65dcef
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 13:47:12 -0700

    Mock out data object for executive connections

---
+++ src/data/executiveConnections.js
@@ -0,0 +1,27 @@
+const executiveConnections = {
+  exe_1_inhibition: {},
+  exe_1_orient: {},
+  exe_2a_shifting: {},
+  exe_2a_working: {},
+  exe_2b_flexibility: {},
+  exe_2b_general: {},
+  exe_2b_global: {},
+  exe_2b_spatial: {},
+  exe_2b_verbal: {},
+  exe_3_initiation: {},
+  exe_3_monitoring: {},
+  exe_3_planning: {},
+  exe_3_sequencing: {},
+  exe_4a_future: {},
+  exe_4a_internalized: {},
+  exe_4a_problem: {},
+  exe_4a_reflective: {},
+  exe_4a_spatial: {},
+  exe_4b_effortful: {},
+  exe_4b_motivational: {},
+  exe_4b_self_compassion: {},
+  exe_4b_self_control: {},
+  exe_4b_self_regulation: {}
+}
+
+export default executiveConnections;
