ff00f8d85cbd183f8ce5652a833d1076c63963d3
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 20:23:35 -0700

    Add try/catch to addClinicInfoToUser

--- src/helpers/addClinicInfoToUser/addClinicInfoToUser.js
+++ src/helpers/addClinicInfoToUser/addClinicInfoToUser.js
@@ -1,21 +1,28 @@
 import getKeyFromLS from '../getKeyFromLS';
 
 const addClinicInfoToUser = async (clinic, userId) => {
-  const response = await fetch(`http://localhost:3000/api/v1/users/${userId}`, {
-    method: 'PUT',
-    headers: {
-      "x-token": getKeyFromLS(),
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      clinic: clinic.name,
-      clinic_abbreviation: clinic.abbreviation,
-      clinic_id: clinic.clinic_id
-    })
-  });
-  const jsonResponse = await response.json();
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/v1/users/${userId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'x-token': getKeyFromLS(),
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          clinic: clinic.name,
+          clinic_abbreviation: clinic.abbreviation,
+          clinic_id: clinic.clinic_id
+        })
+      }
+    );
+    const jsonResponse = await response.json();
 
-  return jsonResponse;
-}
+    return jsonResponse;
+  } catch (error) {
+    throw new Error(`Error updating user by user id ${userId}: ${error}`);
+  }
+};
 
 export default addClinicInfoToUser;
