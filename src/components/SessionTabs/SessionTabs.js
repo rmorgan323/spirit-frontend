3aa81ba0a9f39e900c19b217db72d817d6633495
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-21 06:22:34 -0700

    Build SessionTabs component to render all NavLinks using map array prototype

--- src/components/SessionTabs/SessionTabs.js
+++ src/components/SessionTabs/SessionTabs.js
@@ -3,65 +3,45 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import './SessionTabs.css';
 
-const SessionTabs = () => {
-  const navLinks = [ 'sam', 'modulation', 'postural', 'sensory', 'social', 'executive' ]
+const SessionTabs = props => {
+  const { selectedProcess } = props;
+  const navNames = [
+    'sam',
+    'modulation',
+    'postural',
+    'sensory',
+    'social',
+    'executive'
+  ];
 
-  return (
-    <div className="SessionTabs">
-      <NavLink
-        to="/spirit/sessions/:sessionId/sam"
-        className='session-tabs session-tab-sam'
-      >
-        <h2>SAM</h2>
-      </NavLink>
+  const navHeader = [
+    'SAM',
+    'Modulation',
+    'Postural',
+    'Sensory',
+    'Social',
+    'Executive'
+  ];
 
+  const navLinks = navNames.map((link, index) => {
+    const currentClass = selectedProcess[link]
+      ? `session-tabs session-tab-${link} thread-connection`
+      : `session-tabs session-tab-${link}`;
+    return (
       <NavLink
-        to="/spirit/sessions/:sessionId/modulation"
-        className='session-tabs session-tab-modulation'
+        to={`/spirit/sessions/:sessionId/${link}`}
+        className={currentClass}
       >
-        <h2>Modulation</h2>
+        <h2>{navHeader[index]}</h2>
       </NavLink>
+    );
+  });
 
-      <NavLink
-        to="/spirit/sessions/:sessionId/postural"
-        className='session-tabs session-tab-postural'
-      >
-        <h2>Postural</h2>
-      </NavLink>
-
-      <NavLink
-        to="/spirit/sessions/:sessionId/sensory"
-        className='session-tabs session-tab-sensory'
-      >
-        <h2>Sensory</h2>
-      </NavLink>
-
-      <NavLink
-        to="/spirit/sessions/:sessionId/social"
-        className='session-tabs session-tab-social'
-      >
-        <h2>Social</h2>
-      </NavLink>
-
-      <NavLink
-        to="/spirit/sessions/:sessionId/executive"
-        className='session-tabs session-tab-executive'
-      >
-        <h2>Executive</h2>
-      </NavLink>
-
-      <NavLink
-        to="/spirit/sessions/:sessionId/finish"
-        className='session-tabs session-tab-finish'
-      >
-        <h2>Finish</h2>
-      </NavLink>
-    </div>
-  );
+  return <div className="SessionTabs">{navLinks}</div>;
 };
 
 export const mapStateToProps = store => ({
   selectedProcess: store.selectedProcess
-})
+});
 
 export default connect(mapStateToProps, null)(SessionTabs);
