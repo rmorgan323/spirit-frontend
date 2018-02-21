c5eee8a7aa44ee275688dfd7a955887d06c8fde0
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 17:21:34 -0700

    Build getThreadConnections helper file and directory

---
+++ src/helpers/getThreadConnections/getThreadConnections.js
@@ -0,0 +1,20 @@
+import modulationConnections from '../../data/modulationConnections';
+import posturalConnections from '../../data/posturalConnections';
+import sensoryConnections from '../../data/sensoryConnections';
+import socialConnections from '../../data/socialConnections';
+import executiveConnections from '../../data/executiveConnections';
+
+const getThreadConnections = processComponent => {
+  const SAM = {
+    mod: modulationConnections,
+    pos: posturalConnections,
+    sen: sensoryConnections,
+    soc: socialConnections,
+    exe: executiveConnections
+  };
+
+  const category = processComponent.split('_')[0];
+  const threadConnections = SAM[category][processComponent];
+
+  return threadConnections;
+};
