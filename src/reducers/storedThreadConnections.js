27cc31b5d83c67b0540f8f0803c282e4436f18c1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 17:42:48 -0700

    Add UPDATE_THREAD_CONNECTIONS case to storedThreadConnections reducer

--- src/reducers/storedThreadConnections.js
+++ src/reducers/storedThreadConnections.js
@@ -2,8 +2,17 @@ import markedThreadConnections from '../data/markedThreadConnections';
 
 const storedThreadConnections = (store = markedThreadConnections, action) => {
   switch (action.type) {
-    default:
-      return store;
+  case 'UPDATE_THREAD_CONNECTIONS': {
+    let newConnections = store;
+
+    action.threadConnections.forEach(
+      connection => (newConnections[connection] = true)
+    );
+
+    return newConnections;
+  }
+  default:
+    return store;
   }
 };
 
