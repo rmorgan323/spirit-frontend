3c613fe8378e2d029eb9228cf523016c4424d467
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-20 18:09:15 -0700

    Build CHECK_THREAD_CONNECTIONS case in storedThreadConnections reducer

--- src/reducers/storedThreadConnections.js
+++ src/reducers/storedThreadConnections.js
@@ -2,6 +2,19 @@ import markedThreadConnections from '../data/markedThreadConnections';
 
 const storedThreadConnections = (store = markedThreadConnections, action) => {
   switch (action.type) {
+  case 'CHECK_THREAD_CONNECTIONS': {
+    let selectedConnections = store;
+    const processKeys = Object.keys(action.selectedProcess);
+
+    processKeys.forEach(component => {
+      if (action.selectedProcess[component] !== null) {
+        selectedConnections[component] = true;
+      }
+    })
+
+    return selectedConnections;
+  }
+
   case 'UPDATE_THREAD_CONNECTIONS': {
     let newConnections = store;
 
@@ -11,6 +24,7 @@ const storedThreadConnections = (store = markedThreadConnections, action) => {
 
     return newConnections;
   }
+
   default:
     return store;
   }
