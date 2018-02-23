cf99d3c73b9d7f137bc0cf16c719e06c2a98f3af
Rob Morgan <rmorgan323@gmail.com>
2018-02-22 20:02:40 -0700

    postSession tests

---
+++ src/helpers/postSession/postSession.test.js
@@ -0,0 +1,34 @@
+import postSession from './postSession';
+import { mockApiResponse } from '../../data/mockData/mockApiResponse';
+
+global.localStorage = {
+  getItem: () => null
+};
+
+describe('postSession tests', () => {
+  window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockApiResponse)
+    })
+  );
+
+  it('should be a function', () => {
+    expect(postSession).toBeAFunction;
+  });
+
+  it('postSession is called with the correct params', async () => {
+    const expected = [
+      'http://localhost:3000/api/v1/primary-concerns/1/sessions',
+      {
+        method: 'POST',
+        headers: {
+          'x-token': null,
+          'Content-Type': 'application/json'
+        }
+      }
+    ];
+
+    await postSession(1);
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+});
