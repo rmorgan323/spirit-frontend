c6ad0b2ce078d82c50d8aca391cda50796c98929
Rob Morgan <rmorgan323@gmail.com>
2018-02-22 19:41:16 -0700

    loadTherapyGoalBySession tests

---
+++ src/helpers/loadTherapyGoalBySession/loadTherapyGoalBySession.test.js
@@ -0,0 +1,34 @@
+import loadTherapyGoalBySession from './loadTherapyGoalBySession';
+import { mockApiResponse } from '../../data/mockData/mockApiResponse';
+
+global.localStorage = {
+  getItem: () => null
+};
+
+describe('loadTherapyGoalBySession tests', () => {
+  window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockApiResponse)
+    })
+  );
+
+  it('should be a function', () => {
+    expect(loadTherapyGoalBySession).toBeAFunction;
+  });
+
+  it('loadTherapyGoalBySession is called with the correct params', async () => {
+    const expected = [
+      'http://localhost:3000/api/v1/sessions/1/therapy-goals',
+      {
+        method: 'GET',
+        headers: {
+          'x-token': null,
+          'Content-Type': 'application/json'
+        }
+      }
+    ];
+
+    await loadTherapyGoalBySession(1);
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+});
