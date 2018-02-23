f0ce7c1ae12dfcaddf9e9ea5b374d200bb11f8e0
Rob Morgan <rmorgan323@gmail.com>
2018-02-22 19:24:22 -0700

    loadProcessBySession tests

---
+++ src/helpers/loadProcessBySession/loadProcessBySession.test.js
@@ -0,0 +1,34 @@
+import loadProcessBySession from './loadProcessBySession';
+import { mockApiResponse } from '../../data/mockData/mockApiResponse';
+
+global.localStorage = {
+  getItem: () => null
+};
+
+describe('loadProcessBySession tests', () => {
+  window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockApiResponse)
+    })
+  );
+
+  it('should be a function', () => {
+    expect(loadProcessBySession).toBeAFunction;
+  });
+
+  it('loadProcessBySession is called with the correct params', async () => {
+    const expected = [
+      'http://localhost:3000/api/v1/sessions/1/processes', 
+      {
+        method: 'GET',
+        headers: {
+          'x-token': null,
+          'Content-Type': 'application/json'
+        }
+      }
+    ];
+
+    await loadProcessBySession(1);
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+});
