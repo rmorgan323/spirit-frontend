873b8a30660c874e2ae1942d7fdb67ff32e7f0c2
Rob Morgan <rmorgan323@gmail.com>
2018-02-22 19:44:55 -0700

    loadUser tests

---
+++ src/helpers/loadUser/loadUser.test.js
@@ -0,0 +1,34 @@
+import loadUser from './loadUser';
+import { mockApiResponse } from '../../data/mockData/mockApiResponse';
+
+global.localStorage = {
+  getItem: () => null
+};
+
+describe('loadUser tests', () => {
+  window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockApiResponse)
+    })
+  );
+
+  it('should be a function', () => {
+    expect(loadUser).toBeAFunction;
+  });
+
+  it('loadUser is called with the correct params', async () => {
+    const expected = [
+      'http://localhost:3000/api/v1/users/',
+      {
+        method: 'GET',
+        headers: {
+          'x-token': null,
+          'Content-Type': 'application/json'
+        }
+      }
+    ];
+
+    await loadUser();
+    expect(window.fetch).toHaveBeenCalledWith(...expected);
+  });
+});
