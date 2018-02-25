410a7138c3990cdf86b766ec17dacd0aeb7ec346
Rob Morgan <rmorgan323@gmail.com>
2018-02-24 22:02:53 -0700

    Sensory tests

---
+++ src/components/Sensory/Sensory.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sensory from './Sensory';
+import { shallow } from 'enzyme';
+
+describe('Sensory tests', () => {
+  it('should match snapshot', () => {
+    const renderedApp = shallow(<Sensory />)
+
+    expect(renderedApp).toMatchSnapshot();
+  });
+});
