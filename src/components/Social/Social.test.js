d7e64f2fcf60186c59d2bacbf15539d849e2eae1
Rob Morgan <rmorgan323@gmail.com>
2018-02-24 22:02:17 -0700

    Social tests

---
+++ src/components/Social/Social.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Social from './Social';
+import { shallow } from 'enzyme';
+
+describe('Social tests', () => {
+  it('should match snapshot', () => {
+    const renderedApp = shallow(<Social />)
+
+    expect(renderedApp).toMatchSnapshot();
+  });
+});
