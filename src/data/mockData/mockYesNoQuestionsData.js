c5db6f0e33f6f0a307cf5136b316707acbee0c8b
Rob Morgan <rmorgan323@gmail.com>
2018-02-24 23:39:12 -0700

    YesNoQuestions tests

---
+++ src/data/mockData/mockYesNoQuestionsData.js
@@ -0,0 +1,7 @@
+export const mockTitle = 'Core Development';
+export const mockQuestionsArray = [
+  'Outer Core', 'Inner Core'
+];
+export const mockDatabaseNamesArray = [
+  'pos_1_outer', 'pos_1_inner'
+];
