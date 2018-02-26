4e56f15e6f4bcd1db1af9c64c8791d8240ad0803
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 20:57:41 -0700

    Mock out sessions reducers tests

---
+++ src/reducers/sessions/sessions.test.js
@@ -0,0 +1,13 @@
+import sessions from './sessions';
+import { mockSession } from '../../data/mockData/mockSession';
+import * as actions from '../../actions';
+
+describe('Sessions reducer tests', () => {
+  it('Should return store by default', () => {
+
+  });
+
+  it('Should return a new store with sessions', () => {
+
+  });
+});
