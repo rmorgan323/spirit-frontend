e1d458ddbb115b4c8dd97bfc52bb1310663a8895
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 20:48:36 -0700

    Mock out storedThreadConnections reducer tests

---
+++ src/reducers/storedThreadConnections/storedThreadConnections.test.js
@@ -0,0 +1,13 @@
+import storedThreadConnections from './storedThreadConnections';
+import { mockStoredThreadConnections } from '../../data/mockData/mockStoredThreadConnections';
+import * as actions from '../../actions';
+
+describe('storedThreadConnections reducer tests', () => {
+  it('Should return store by default', () => {
+
+  });
+
+  it('Should return a new store with storedThreadConnections', () => {
+
+  });
+});
