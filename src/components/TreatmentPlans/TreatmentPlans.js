35ebc06c8cdb7bb67ef1dbbfc6a9cd61e0623b8a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-26 14:49:08 -0700

    Refactor TreatmentPlans component

--- src/components/TreatmentPlans/TreatmentPlans.js
+++ src/components/TreatmentPlans/TreatmentPlans.js
@@ -17,117 +17,64 @@ class TreatmentPlans extends Component {
 
   render() {
     const { currentTab } = this.state;
+    const tabHeaderTitles = [
+      'Sensory Modulation',
+      'Postural',
+      'Sensory Discrimination',
+      'Social/Emotional Development',
+      'Executive Functioning'
+    ];
+
+    const tabHeaderClass = [
+      'modulation',
+      'postural',
+      'sensory',
+      'social',
+      'executive'
+    ];
+
+    const treatmentModuleTypes = [
+      'modulation_',
+      'postural_',
+      'sensory_discrimination_',
+      'social_emotional_',
+      'executive_functioning_'
+    ];
+
+    const tabHeaders = tabHeaderClass.map((className, index) => {
+      return (
+        <div
+          className={
+            currentTab === className
+              ? `treatment-header treatment-header-active treatment-header-${className}`
+              : `treatment-header treatment-header-${className}`
+          }
+          onClick={() => this.handleTabChange(className)}
+        >
+          <h4>{tabHeaderTitles[index]}</h4>
+        </div>
+      );
+    });
+
+    const treatmentModules = treatmentModuleTypes.map((type, index) => {
+      return (
+        <div
+          className={
+            currentTab === tabHeaderClass[index]
+              ? 'treatment-body'
+              : 'treatment-body display-none'
+          }
+        >
+          <TreatmentPlanModule type={type} />
+        </div>
+      );
+    });
 
     return (
       <div className="TreatmentPlans">
-        <div className="treatment-header-tabs">
-          <div
-            className={
-              currentTab === 'modulation'
-                ? 'treatment-header treatment-header-active treatment-header-modulation'
-                : 'treatment-header treatment-header-modulation'
-            }
-            onClick={() => this.handleTabChange('modulation')}
-          >
-            <h4>Modulation</h4>
-          </div>
+        <div className="treatment-header-tabs">{tabHeaders}</div>
 
-          <div
-            className={
-              currentTab === 'postural'
-                ? 'treatment-header treatment-header-active'
-                : 'treatment-header'
-            }
-            onClick={() => this.handleTabChange('postural')}
-          >
-            <h4>Postural</h4>
-          </div>
-
-          <div
-            className={
-              currentTab === 'sensory'
-                ? 'treatment-header treatment-header-active'
-                : 'treatment-header'
-            }
-            onClick={() => this.handleTabChange('sensory')}
-          >
-            <h4>Sensory Discrimination</h4>
-          </div>
-
-          <div
-            className={
-              currentTab === 'social'
-                ? 'treatment-header treatment-header-active'
-                : 'treatment-header'
-            }
-            onClick={() => this.handleTabChange('social')}
-          >
-            <h4>Social/Emotional Development</h4>
-          </div>
-
-          <div
-            className={
-              currentTab === 'executive'
-                ? 'treatment-header treatment-header-active treatment-header-executive'
-                : 'treatment-header treatment-header-executive'
-            }
-            onClick={() => this.handleTabChange('executive')}
-          >
-            <h4>Executive Functioning</h4>
-          </div>
-        </div>
-
-        <div className="module-container">
-          <div
-            className={
-              currentTab === 'modulation'
-                ? 'treatment-body'
-                : 'treatment-body display-none'
-            }
-          >
-            <TreatmentPlanModule type={'modulation_'} />
-          </div>
-
-          <div
-            className={
-              currentTab === 'postural'
-                ? 'treatment-body'
-                : 'treatment-body display-none'
-            }
-          >
-            <TreatmentPlanModule type={'posture_'} />
-          </div>
-
-          <div
-            className={
-              currentTab === 'sensory'
-                ? 'treatment-body'
-                : 'treatment-body display-none'
-            }
-          >
-            <TreatmentPlanModule type={'sensory_discrimination_'} />
-          </div>
-
-          <div
-            className={
-              currentTab === 'social'
-                ? 'treatment-body'
-                : 'treatment-body display-none'
-            }
-          >
-            <TreatmentPlanModule type={'social_emotional_'} />
-          </div>
-
-          <div
-            className={
-              currentTab === 'executive'
-                ? 'treatment-body'
-                : 'treatment-body display-none'
-            }
-          >
-            <TreatmentPlanModule type={'executive_functioning_'} />
-          </div>
-        </div>
+        <div className="module-container">{treatmentModules}</div>
       </div>
     );
   }
