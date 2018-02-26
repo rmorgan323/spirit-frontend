848f110f12246b92f3523c857b8ec68c7bb74d69
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 20:55:43 -0700

    Complete storedThreadConnections reducer tests

--- src/reducers/storedThreadConnections/storedThreadConnections.test.js
+++ src/reducers/storedThreadConnections/storedThreadConnections.test.js
@@ -1,13 +1,17 @@
 import storedThreadConnections from './storedThreadConnections';
-import { mockStoredThreadConnections } from '../../data/mockData/mockStoredThreadConnections';
+import markedThreadConnections from '../../data/markedThreadConnections';
 import * as actions from '../../actions';
 
 describe('storedThreadConnections reducer tests', () => {
   it('Should return store by default', () => {
+    const expected = markedThreadConnections;
 
+    expect(storedThreadConnections(undefined, markedThreadConnections)).toEqual(expected);
   });
 
   it('Should return a new store with storedThreadConnections', () => {
+    const expected = markedThreadConnections;
 
+    expect(storedThreadConnections(undefined, actions.checkThreadConnections(markedThreadConnections))).toEqual(expected)
   });
 });
