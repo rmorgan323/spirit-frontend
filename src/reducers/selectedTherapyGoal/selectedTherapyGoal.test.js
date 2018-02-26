1a2743cc3090e9bd72b3c6477b2f4b54228b5a4e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 21:08:41 -0700

    Mock out selectedTherapyGoal reducer tests

---
+++ src/reducers/selectedTherapyGoal/selectedTherapyGoal.test.js
@@ -0,0 +1,13 @@
+import selectedTherapyGoal from './selectedTherapyGoal';
+import { mockTherapyGoal } from '../../data/mockData/mockTherapyGoal';
+import * as actions from '../../actions';
+
+describe('selectedTherapyGoal reducer tests', () => {
+  it('Should return store by default', () => {
+
+  });
+
+  it('Should return a new store with a selectedTherapyGoal', () => {
+
+  });
+});
