cd362917890bbce516cc81ff0f8570e84634c2a1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 20:45:52 -0700

    Complete user reducer tests

--- src/reducers/user/user.test.js
+++ src/reducers/user/user.test.js
@@ -1,7 +1,17 @@
 import user from './user';
-import mockUser from '../../data/mockData/mockUserData';
+import { mockUserData } from '../../data/mockData/mockUserData';
 import * as actions from '../../actions';
 
 describe('User reducer tests', () => {
-  
+  it('Should return store by default', () => {
+    const expected = {};
+
+    expect(user(undefined, {})).toEqual(expected);
+  });
+
+  it('Should return a new store with a user', () => {
+    const expected = mockUserData;
+
+    expect(user(undefined, actions.userToStore(mockUserData))).toEqual(expected);
+  });
 });
