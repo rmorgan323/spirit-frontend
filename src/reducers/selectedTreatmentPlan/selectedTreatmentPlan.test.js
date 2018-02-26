8055e71b1b98f591c8c46b4721f5603a40a27497
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-25 21:05:03 -0700

    Mock out selectedTreatmentPlan reducer tests

---
+++ src/reducers/selectedTreatmentPlan/selectedTreatmentPlan.test.js
@@ -0,0 +1,13 @@
+import selectedTreatmentPlan from './selectedTreatmentPlan';
+import { mockTreatmentPlan } from '../../data/mockData/mockTreatmentPlan';
+import * as actions from '../../actions';
+
+describe('selectedTreatmentPlan reducer tests', () => {
+  it('Should return store by default', () => {
+
+  });
+
+  it('Should return a new store with a selectedTreatmentPlan', () => {
+
+  });
+});
