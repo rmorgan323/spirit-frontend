5cb5d00a03fcc4b4994f0992aa3ddeeb00302297
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-26 22:10:05 -0700

    Complete Users snapshot/MSP/MDP tests

---
+++ src/containers/Users/Users.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Users, mapStateToProps } from './Users';
+import { shallow } from 'enzyme';
+import { mockUserData } from '../../data/mockData/mockUserData';
+import { mockHistory } from '../../data/mockData/mockHistory';
+
+describe('Users tests', () => {
+  let user;
+  let history;
+  let renderedUsers;
+
+  beforeEach(() => {
+    user = mockUserData;
+    history = mockHistory;
+
+    renderedUsers = shallow(<Users user={user} history={history} />);
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderedUsers).toMatchSnapshot();
+  });
+});
+
+describe('mapStateToProps tests', () => {
+  it('Should pull a user from store', () => {
+    const user = mockUserData;
+    const mockStore = { user };
+    const result = mapStateToProps(mockStore);
+
+    expect(result.user).toEqual(mockStore.user);
+  });
+});
