d793b20f3c296ea55b0fd07dcc55d75e5319b761
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-27 11:09:50 -0700

    Complete TreatmentPlans test suite

--- src/components/TreatmentPlans/TreatmentPlans.test.js
+++ src/components/TreatmentPlans/TreatmentPlans.test.js
@@ -8,4 +8,22 @@ describe('TreatmentPlans tests', () => {
 
     expect(renderedTreatmentPlans).toMatchSnapshot();
   });
+
+  it('Should change state when a new tab is clicked', () => {
+    const renderedTreatmentPlans = shallow(<TreatmentPlans />);
+    const tabHeaders = [
+      'postural',
+      'sensory',
+      'social',
+      'executive',
+      'modulation'
+    ];
+
+    expect(renderedTreatmentPlans.state().currentTab).toEqual('modulation');
+
+    tabHeaders.forEach(tab => {
+      renderedTreatmentPlans.instance().handleTabChange(tab);
+      expect(renderedTreatmentPlans.state().currentTab).toEqual(tab);
+    });
+  });
 });
