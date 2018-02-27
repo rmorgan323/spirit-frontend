23019b9eb0bdbb42a16136ea85f40a830d807ed1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-26 18:42:36 -0700

    Complete Concerns snapshot test and MSP tests

---
+++ src/containers/Concerns/Concerns.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Concerns, mapStateToProps } from './Concerns';
+import { shallow } from 'enzyme';
+import { mockConcern } from '../../data/mockData/mockConcern';
+
+describe('Concerns tests', () => {
+  let patientConcerns;
+  let renderedConcerns;
+
+  beforeEach(() => {
+    patientConcerns = [mockConcern];
+
+    renderedConcerns = shallow(<Concerns patientConcerns={patientConcerns} />);
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderedConcerns).toMatchSnapshot();
+  });
+});
+
+describe('mapStateToProps tests', () => {
+  it('Should pull patientConcerns from store', () => {
+    const mockStore = { mockConcern };
+    const result = mapStateToProps(mockStore);
+
+    expect(result.patientConcerns).toEqual(mockStore.patientConcerns);
+  });
+});
