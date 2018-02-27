956639fb76115c7e22508dae75f14faa61003ad9
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-26 18:51:24 -0700

    Complete Definition snapshot test and MSP

---
+++ src/containers/Definition/Definition.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Definition, mapStateToProps } from './Definition';
+import { shallow } from 'enzyme';
+import { mockDefinitions } from '../../data/mockData/mockDefinitions';
+
+describe('Definition tests', () => {
+  let definitions;
+  let renderedDefinition;
+  let title;
+
+  beforeEach(() => {
+    definitions = mockDefinitions;
+    title = 'S-A-M';
+
+    renderedDefinition = shallow(
+      <Definition definitions={definitions} title={title} />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderedDefinition).toMatchSnapshot();
+  });
+});
+
+describe('mapStateToProps tests', () => {
+  it('Should pull definitions from store', () => {
+    const mockStore = { mockDefinitions };
+    const result = mapStateToProps(mockStore);
+
+    expect(result.definitions).toEqual(mockStore.definitions);
+  });
+});
