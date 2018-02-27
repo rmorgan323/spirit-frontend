2ab52f240638e6ffdb38a2f646ac16981530d4e8
Rob Morgan <rmorgan323@gmail.com>
2018-02-26 22:14:45 -0700

    processProcessesData tests

---
+++ src/helpers/processProcessesData/processProcessesData.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import processProcessesData from './processProcessesData';
+import { mockProcess } from '../../data/mockData/mockProcess';
+
+
+describe('processProcessesData tests', () => {
+  it('should be a function', () => {
+    expect(processProcessesData).toBeAFunction;
+  });
+
+
+  it('should return an object', () => {
+    const mockReturn = processProcessesData([mockProcess])
+
+    expect(typeof mockReturn).toEqual('object');
+  });
+
+  it('should add a value to the values property array in each object, when available', () => {
+    const mockReturn = processProcessesData([mockProcess])
+
+    expect(mockReturn.postural[1].values).toEqual(['7a']);
+  });
+
+  it('The value property should remain null if values are not supplied', () => {
+    const mockReturn = processProcessesData([mockProcess])
+
+    expect(mockReturn.sensory[2].values).toEqual([null]);
+  });
+});
