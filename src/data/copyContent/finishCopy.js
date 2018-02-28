8ca058c88865e9ab2035f0d408f0951ffc6670a9
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-27 21:49:53 -0700

    Build finishCopy data object and call in Finish component

--- src/data/copyContent/finishCopy.js
+++ src/data/copyContent/finishCopy.js
@@ -1,3 +1,7 @@
 const finishCopy = {
+  recommend: 'We recommend reviewing your session results before writing plans and goals',
+  stepsi: 'S.T.E.P.S.I. ©',
+  copm: 'Modified from the C.O.P.M.'
+};
 
-}
+export default finishCopy;
