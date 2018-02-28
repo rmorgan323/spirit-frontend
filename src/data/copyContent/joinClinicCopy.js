a0c1aa8abfa24f1859710506c706ad087943287b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-27 21:58:28 -0700

    Build joinClinicCopy object and call in Join component

--- src/data/copyContent/joinClinicCopy.js
+++ src/data/copyContent/joinClinicCopy.js
@@ -1,7 +1,8 @@
 /*eslint-disable max-len*/
 
 const joinClinicCopy = {
-
+  directions1: `To join an existing clinic on SpIRiT©, you must use a clinic's eight digit passcode. If you do not know your clinic's passcode, please contact your SpIRiT© administrator.`,
+  directions2: `If your clinic has not been registered, click 'CREATE NEW CLINIC' below to learn how you can add your clinic and start using SpIRiT©.`
 };
 
 export default joinClinicCopy;
