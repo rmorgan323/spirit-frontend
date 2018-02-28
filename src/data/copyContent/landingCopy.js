18ad20689f96f4f6d4b1137368efd1ecb3a833a4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-27 22:03:47 -0700

    Build landingCopy object and call in Spirit component

--- src/data/copyContent/landingCopy.js
+++ src/data/copyContent/landingCopy.js
@@ -1,5 +1,7 @@
 /*eslint-disable max-len*/
 
-const landingCopy = {};
+const landingCopy = {
+  descriptor: `A clinical reasoning model for ensuring best practice in pediatric occupational therapy.`
+};
 
 export default landingCopy;
