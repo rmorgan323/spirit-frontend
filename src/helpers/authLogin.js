9a1b50e483cc83af7961dc12748049eade83d48c
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-07 12:13:21 -0600

    Add authLogin for production

---
+++ src/helpers/authLogin.js
@@ -0,0 +1,4 @@
+const authLogin = 'https://spirit.e1.loginrocket.com/login'; // Live login
+// const authLogin = 'https://spirit-prod.e1.loginrocket.com/'; // Production login
+
+export default authLogin;
