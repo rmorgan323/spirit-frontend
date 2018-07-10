1e9bef8d4df3980796f51c9a601ec86433661064
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-10 07:38:44 -0600

    Adjust notes to Production/Dev Root

--- src/helpers/apiRoot.js
+++ src/helpers/apiRoot.js
@@ -1,5 +1,5 @@
 
-// const apiRoot = 'https://spirit-be.herokuapp.com'; // Live root
-const apiRoot = 'http://localhost:3000'; // Production root
+// const apiRoot = 'https://spirit-be.herokuapp.com'; // Production root
+const apiRoot = 'http://localhost:3000'; // Dev root
 
 export default apiRoot;
