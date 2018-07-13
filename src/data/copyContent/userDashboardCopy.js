c2f9197ab89a1d625ab7b9771b06528ebc74b950
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-13 12:51:27 -0600

    Add welcome copy

--- src/data/copyContent/userDashboardCopy.js
+++ src/data/copyContent/userDashboardCopy.js
@@ -1,6 +1,8 @@
 /*eslint-disable max-len*/
 
 const userDashboardCopy = {
+  welcomeDirections1: `To start tracking your patients through SpIRiT, add a new clinic or join an existing one below.`,
+  welcomeDirections2: `Joining an existing clinic will require a passcode which you can obtain from your your administrator.`,
   patientDirections1: `Each patient name is a combination of your clinic abbreviation, the patient's first and last initial, and a unique number identifier (ie CLI + AB + 10).`,
   patientDirections2: `Please keep this abstracted name in your personal patient records for future reference.`
 };
