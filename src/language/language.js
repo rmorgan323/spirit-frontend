0fbc878123d0871d478063c97d56ee3fe3fa63ef
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-22 17:05:29 -0600

    Adjust language exports

--- src/language/language.js
+++ src/language/language.js
@@ -1,57 +1,57 @@
-const language = {
-  landing: {
-    descriptor: `A clinical reasoning model for ensuring best practice in pediatric occupational therapy.`
-  },
-  userDashboard: {
-    otDash: (name) => `${name} - OT Dashboard`,
-    welcome: `Welcome to SpIRiT, `,
-    seeClinic: `SEE CLINIC INFO`,
-    addJoinClinic: `ADD/JOIN A CLINIC`,
-    newPatients: `New Patients`,
-    submit: `SUBMIT`,
-    addNewPatientDetails: `Add NEW patient with 2 patient initials`,
-    currentPatients: `Current Patients`,
-    welcomeDirections1: `To start tracking your patients through SpIRiT, add a new clinic or join an existing one below.`,
-    welcomeDirections2: `Joining an existing clinic will require a passcode which you can obtain from your your administrator.`,
-    patientDirections1: `Each patient name is a combination of your clinic abbreviation, the patient's first and last initial, and a unique number identifier (ie CLI + AB + 10).`,
-    patientDirections2: `Please keep this abstracted name in your personal patient records for future reference.`
-  },
-  createClinic: {
-    addDirections1: `To add a new clinic, you must provide a clinic name and three letter abbreviation. This abbreviation will be used to abstract your patient's real name when adding them to SpIRiT©.`,
-    addDirections2: `After submission, you will be provided with a passcode. This passcode will be used by other members in order to join your clinic.`,
-    clinicNameError: `Please enter a clinic name and abbreviation`,
-    clinicAbbreviationError: `Clinic abbreviation must be three characters long`,
-    addClinicSuccess: `Clinic successfully added! Your clinic passcode is `,
-    currentClinic: `Your Current Clinic`,
-    memberOfClinic: `You are a member of: `,
-    clinicAbbreviation: `Clinic Abbreviation: `,
-    clinicPasscode: `Clinic Passcode: `,
-    newClinicPlaceholder: `New Clinic Name"`,
-    clinicAbbreviationPlaceholder: `Choose a 3-letter abbreviation for your clinic`,
-    submitClinic: `SUBMIT`,
-    joinClinic: `JOIN EXISTING CLINIC`
-  },
-  joinClinic: {
-    directions1: `To join an existing clinic on SpIRiT©, you must use a clinic's eight digit passcode. If you do not know your clinic's passcode, please contact your SpIRiT© administrator.`,
-    directions2: `If your clinic has not been registered, click 'CREATE NEW CLINIC' below to learn how you can add your clinic and start using SpIRiT©.`
-  },
-  bodyDiagram: {
-    title: `Body Parts`,
-    coreAbdominal: `1. Core Abdominal Control`,
-    shoulder: `2. Shoulder Girdle Core`,
-    pelvic: `3. Pelvic Trunk/LE Core`,
-    head: `4. Head/Neck to Trunk Core`,
-    eyes: `5. Eyes in Head Core`,
-    hand: `6. Hand Control`,
-    lower: `7. Lower Extremity`,
-    foot: `8. Foot/Base of Support Core`
-  },
-  finish: {
-    recommend:
-      'We recommend reviewing your session results before writing plans and goals',
-    stepsi: 'S.T.E.P.S.I. ©',
-    copm: 'Modified from the C.O.P.M.'
-  }
-}
+export const landing = {
+  descriptor: `A clinical reasoning model for ensuring best practice in pediatric occupational therapy.`
+};
 
-export default language
+export const userDashboard = {
+  otDash: name => `${name} - OT Dashboard`,
+  welcome: `Welcome to SpIRiT, `,
+  seeClinic: `SEE CLINIC INFO`,
+  addJoinClinic: `ADD/JOIN A CLINIC`,
+  newPatients: `New Patients`,
+  submit: `SUBMIT`,
+  addNewPatientDetails: `Add NEW patient with 2 patient initials`,
+  currentPatients: `Current Patients`,
+  welcomeDirections1: `To start tracking your patients through SpIRiT, add a new clinic or join an existing one below.`,
+  welcomeDirections2: `Joining an existing clinic will require a passcode which you can obtain from your your administrator.`,
+  patientDirections1: `Each patient name is a combination of your clinic abbreviation, the patient's first and last initial, and a unique number identifier (ie CLI + AB + 10).`,
+  patientDirections2: `Please keep this abstracted name in your personal patient records for future reference.`
+};
+
+export const createClinic = {
+  addDirections1: `To add a new clinic, you must provide a clinic name and three letter abbreviation. This abbreviation will be used to abstract your patient's real name when adding them to SpIRiT©.`,
+  addDirections2: `After submission, you will be provided with a passcode. This passcode will be used by other members in order to join your clinic.`,
+  clinicNameError: `Please enter a clinic name and abbreviation`,
+  clinicAbbreviationError: `Clinic abbreviation must be three characters long`,
+  addClinicSuccess: `Clinic successfully added! Your clinic passcode is `,
+  currentClinic: `Your Current Clinic`,
+  memberOfClinic: `You are a member of: `,
+  clinicAbbreviation: `Clinic Abbreviation: `,
+  clinicPasscode: `Clinic Passcode: `,
+  newClinicPlaceholder: `New Clinic Name"`,
+  clinicAbbreviationPlaceholder: `Choose a 3-letter abbreviation for your clinic`,
+  submitClinic: `SUBMIT`,
+  joinClinic: `JOIN EXISTING CLINIC`
+};
+export const joinClinic = {
+  directions1: `To join an existing clinic on SpIRiT©, you must use a clinic's eight digit passcode. If you do not know your clinic's passcode, please contact your SpIRiT© administrator.`,
+  directions2: `If your clinic has not been registered, click 'CREATE NEW CLINIC' below to learn how you can add your clinic and start using SpIRiT©.`
+};
+
+export const bodyDiagram = {
+  title: `Body Parts`,
+  coreAbdominal: `1. Core Abdominal Control`,
+  shoulder: `2. Shoulder Girdle Core`,
+  pelvic: `3. Pelvic Trunk/LE Core`,
+  head: `4. Head/Neck to Trunk Core`,
+  eyes: `5. Eyes in Head Core`,
+  hand: `6. Hand Control`,
+  lower: `7. Lower Extremity`,
+  foot: `8. Foot/Base of Support Core`
+};
+
+export const finish = {
+  recommend:
+    'We recommend reviewing your session results before writing plans and goals',
+  stepsi: 'S.T.E.P.S.I. ©',
+  copm: 'Modified from the C.O.P.M.'
+};
