6a731d4ffdb8eba51f1122ae5b2e04a4db44ee2c
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-22 16:51:26 -0600

    Remove copyContent directory in favor of language object

--- src/data/copyContent/landingCopy.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable max-len*/
-
-const landingCopy = {
-  descriptor: `A clinical reasoning model for ensuring best practice in pediatric occupational therapy.`
-};
-
-export default landingCopy;
