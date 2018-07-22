ccd99eb3e661ed499aadfa1476429562be4f1cc0
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-22 17:17:23 -0600

    Fix linting

--- src/language/language.js
+++ src/language/language.js
@@ -32,6 +32,7 @@ export const createClinic = {
   submitClinic: `SUBMIT`,
   joinClinic: `JOIN EXISTING CLINIC`
 };
+
 export const joinClinic = {
   directions1: `To join an existing clinic on SpIRiT©, you must use a clinic's eight digit passcode. If you do not know your clinic's passcode, please contact your SpIRiT© administrator.`,
   directions2: `If your clinic has not been registered, click 'CREATE NEW CLINIC' below to learn how you can add your clinic and start using SpIRiT©.`
