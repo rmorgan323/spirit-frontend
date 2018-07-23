c59128f9aa6dcde626f7efe3c5a4761e43eb43fa
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-22 18:07:06 -0600

    Refactor Modulation to use langauge object

--- src/components/Modulation/Modulation.js
+++ src/components/Modulation/Modulation.js
@@ -1,109 +1,34 @@
 import React from 'react';
 import SessionHeader from '../SessionHeader/SessionHeader';
 import CategorySliders from '../CategorySliders/CategorySliders';
+import { modulation } from '../../language/language';
 import './Modulation.css';
 
 const Modulation = () => {
+  const { header, columns } = modulation;
+
   return (
     <div className="Modulation">
       <SessionHeader
-        title={'Modulation Capacities: Sensory Modulation to Self-Regulation'}
-        headings={[
-          'Vestibular',
-          'Proprioception',
-          'Tactile',
-          'Auditory',
-          'Visual',
-          'Intero'
-        ]}
-        databaseNamesArray={[
-          'mod_h_vestibular',
-          'mod_h_proprioception',
-          'mod_h_tactile',
-          'mod_h_auditory',
-          'mod_h_visual',
-          'mod_h_intero'
-        ]}
+        title={header.title}
+        headings={header.terms}
+        databaseNamesArray={header.db}
       />
 
       <div className="category-content">
-        <CategorySliders
-          title={'Capacities'}
-          slidersArray={['Defensiveness', 'Over', 'Under', 'Variable']}
-          databaseNamesArray={[
-            'mod_1_defensiveness',
-            'mod_1_over',
-            'mod_1_under',
-            'mod_1_variable'
-          ]}
-          reminderAsterisk={[null, '*', '*', '*', '*']}
-          reminder={
-            '* If selected, remember to make a hypothesized arousal chart.'
-          }
-        />
+        {Object.keys(columns).map(column => {
+          const { title, terms, db, reminderPlacement, reminder } = columns[column];
 
-        <CategorySliders
-          title={'Capacities'}
-          slidersArray={[
-            'Attention',
-            'Action',
-            'Affect',
-            'Autonomic',
-            'Arousability'
-          ]}
-          databaseNamesArray={[
-            'mod_2_attention',
-            'mod_2_action',
-            'mod_2_affect',
-            'mod_2_autonomic',
-            'mod_2_arousability'
-          ]}
-          reminderAsterisk={[null, null, null, null, '*']}
-        />
-
-        <CategorySliders
-          title={'Self-Regulation'}
-          slidersArray={[
-            'Physiological',
-            'Sensory',
-            'Motor',
-            'Emotional',
-            'Cognitive',
-            'Social',
-            'Prosocial'
-          ]}
-          databaseNamesArray={[
-            'mod_3_physiological',
-            'mod_3_sensory',
-            'mod_3_motor',
-            'mod_3_emotional',
-            'mod_3_cognitive',
-            'mod_3_social',
-            'mod_3_prosocial'
-          ]}
-        />
-
-        <CategorySliders
-          title={'Regulatory Capacities'}
-          slidersArray={['Passive', 'Co-Regulation', 'Approach', 'Adaptive']}
-          databaseNamesArray={[
-            'mod_4a_passive',
-            'mod_4a_co_regulation',
-            'mod_4a_approach',
-            'mod_4a_adaptive'
-          ]}
-        />
-
-        <CategorySliders
-          title={'Regulatory Capacities'}
-          slidersArray={['Active', 'Self-Regulation', 'Avoid', 'Maladaptive']}
-          databaseNamesArray={[
-            'mod_4b_active',
-            'mod_4b_self_regulation',
-            'mod_4b_avoid',
-            'mod_4b_maladaptive'
-          ]}
-        />
+          return (
+            <CategorySliders
+              title={title}
+              slidersArray={terms}
+              databaseNamesArray={db}
+              reminderAsterisk={reminderPlacement && reminderPlacement}
+              reminder={reminder}
+            />
+          );
+        })}
       </div>
     </div>
   );
