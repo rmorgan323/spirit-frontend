ffd9b5cf183e6d3c7ee502821c9895f8d308cfeb
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-22 19:00:00 -0600

    Add social to language file

--- src/language/language.js
+++ src/language/language.js
@@ -362,8 +362,115 @@ export const sensory = {
 };
 
 export const social = {
-  header: {},
-  columns: {}
+  header: {
+    title: `Social/Emotional Developmental Capacities`
+  },
+  columns: {
+    column1: {
+      title: `DIR FEDLS's ©`,
+      terms: [
+        'Regulation',
+        'Engagement',
+        'Reciprocity',
+        'Problem Solving',
+        'Representational/Symbolic Thinking',
+        'Triangular Thinking',
+        'Comparative Thinking',
+        'Reflective'
+      ],
+      db: [
+        'soc_1_regulation',
+        'soc_1_engagement',
+        'soc_1_reciprocity',
+        'soc_1_problem',
+        'soc_1_representation',
+        'soc_1_triangular',
+        'soc_1_comparative',
+        'soc_1_reflective'
+      ]
+    },
+    column2: {
+      title: 'Implicit/Explicit',
+      terms: [],
+      db: []
+    },
+    column3: {
+      title: 'Social Cognition Core Components',
+      terms: [
+        'Self/Other Awareness',
+        'Joint Attention',
+        'Social Referencing',
+        'Imitation',
+        'Inferential Thinking/Intention',
+        'Contingent Interaction/Reciprocal Actions',
+        'Theory of Mind',
+        'Central Coherence',
+        'Mentalizing',
+        'Empathy',
+        'Social Perspective',
+        'Social Motivators AAAA',
+        'Pro-Social Motivation - Moral'
+      ],
+      db: [
+        'soc_2_self_awareness',
+        'soc_2_joint',
+        'soc_2_social_referencing',
+        'soc_2_imitation',
+        'soc_2_inferential',
+        'soc_2_contingent',
+        'soc_2_theory',
+        'soc_2_central',
+        'soc_2_mentalizing',
+        'soc_2_empathy',
+        'soc_2_social_perspective',
+        'soc_2_social_motivators',
+        'soc_2_pro_social'
+      ]
+    },
+    column4: {
+      title: 'Emotional Processing',
+      terms: [
+        'Affective/Valence Response',
+        'Emotional Contagion',
+        'Activation of Low-Routes',
+        'Emotional Cascade',
+        'Activation of High-Routes',
+        'Emotional Differentiation',
+        'Emotional Association and Memory',
+        'Self-Control',
+        'Self-Regulation',
+        'Self-Compassion'
+      ],
+      db: [
+        'soc_3_affective',
+        'soc_3_emotional_contagion',
+        'soc_3_activation_low',
+        'soc_3_emotional_cascade',
+        'soc_3_activation_high',
+        'soc_3_emotional_differentiation',
+        'soc_3_emotional_association',
+        'soc_3_self_control',
+        'soc_3_self_regulation',
+        'soc_3_self_compassion'
+      ]
+    },
+    column5: {
+      type: 'YesNo',
+      title: 'Other Questions',
+      terms: [
+        'Mental Health Support Indicated?',
+        'Thoughts and/or emotions require intervention or medication?',
+        'Attachment related care needed?',
+        'Trauma informed care needed?'
+      ],
+      db: [
+        'soc_4_mental',
+        'soc_4_thoughts',
+        'soc_4_attachments',
+        'soc_4_trauma'
+      ]
+    }
+  }
 };
 
 export const executive = {
