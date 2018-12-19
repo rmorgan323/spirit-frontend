e50d29ce3105c9c4c52138f6414b81c78ac556bb
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-12-19 07:12:16 -0700

    Refine methods in Routes CDM to avoid redirecting if no user found

--- src/containers/Routes/Routes.js
+++ src/containers/Routes/Routes.js
@@ -30,9 +30,12 @@ import Finish from '../Finish/Finish';
 
 export class Routes extends Component {
   componentDidMount = async () => {
-    await this.checkForKey();
-    await this.props.getUser();
-    await this.props.getDefinitions();
+    const hasExistingUser = await this.checkForKey();
+
+    if (hasExistingUser) {
+      await this.props.getUser();
+      await this.props.getDefinitions();
+    }
   };
 
   checkForKey = () => {
@@ -40,12 +43,12 @@ export class Routes extends Component {
     if (window.location.search.length > 200) {
       const token = this.getToken();
       localStorage.setItem('spirit-token-987', token);
+      return true;
     } else if (key) {
-      return;
+      return true;
     } else {
       clearLocalStorage();
-      window.location = authLogin;
-      return;
+      return false;
     }
   };
 
@@ -102,7 +105,7 @@ export class Routes extends Component {
             component={Finish}
           />
 
-          <Route 
+          <Route
             path="/spirit/sessions/:session_id/view"
             component={ViewSession}
           />
@@ -149,7 +152,10 @@ export const mapDispatchToProps = dispatch => ({
   }
 });
 
-export default connect(null, mapDispatchToProps)(Routes);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Routes);
 
 Routes.propTypes = {
   getUser: PropTypes.func,
