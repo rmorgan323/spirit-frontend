a21c9a1142abde0e8a4fc6d6fd020500260b7eaf
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-12-20 08:42:08 -0700

    Update processProcessesData tests to reflect sorting and filtering of null values

--- src/helpers/processProcessesData/processProcessesData.test.js
+++ src/helpers/processProcessesData/processProcessesData.test.js
@@ -15,12 +15,6 @@ describe('processProcessesData tests', () => {
   it('should add a value to the values property array in each object, when available', () => {
     const mockReturn = processProcessesData([mockProcess]);
 
-    expect(mockReturn.postural[1].values).toEqual(['7A']);
-  });
-
-  it('The value property should remain null if values are not supplied', () => {
-    const mockReturn = processProcessesData([mockProcess]);
-
-    expect(mockReturn.sensory[2].values).toEqual([null]);     
+    expect(mockReturn.postural[1].values).toEqual(['7R']);
   });
 });
