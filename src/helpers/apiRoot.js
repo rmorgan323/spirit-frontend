13946eed59a69eb789acebf0aac3a5f761487f65
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-12-21 07:48:09 -0700

    Switch apiRoote/authLogin back to production variables

--- src/helpers/apiRoot.js
+++ src/helpers/apiRoot.js
@@ -1,5 +1,5 @@
 
-// const apiRoot = 'https://spirit-be.herokuapp.com'; // Production root
-const apiRoot = 'http://localhost:3000'; // Dev root
+const apiRoot = 'https://spirit-be.herokuapp.com'; // Production root
+// const apiRoot = 'http://localhost:3000'; // Dev root
 
 export default apiRoot;
--- src/helpers/authLogin.js
+++ src/helpers/authLogin.js
@@ -1,4 +1,4 @@
-// const authLogin = 'https://spirit.e1.loginrocket.com/login'; // Production login
-const authLogin = 'https://spirit-dev.e1.loginrocket.com/'; // Dev login
+const authLogin = 'https://spirit.e1.loginrocket.com/login'; // Production login
+// const authLogin = 'https://spirit-dev.e1.loginrocket.com/'; // Dev login
 
 export default authLogin;
